b44bc4f8f5ce8207959df84177a0056ff7156400
Nicholas Marriott <nicholas.marriott@gmail.com>
2007-11-23 14:28:47 +0000

    Forgot to commit.

---
+++ cmd-copy-mode.c
@@ -0,0 +1,55 @@
+/* $Id: cmd-copy-mode.c,v 1.1 2007-11-23 14:28:47 nicm Exp $ */
+
+/*
+ * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/types.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "tmux.h"
+
+/*
+ * Enter copy mode. Only valid when bound to a key.
+ */
+
+void	cmd_copy_mode_exec(void *, struct cmd_ctx *);
+
+const struct cmd_entry cmd_copy_mode_entry = {
+	"copy-mode", NULL, "",
+	CMD_NOCLIENT,
+	NULL,
+	cmd_copy_mode_exec, 
+	NULL,
+	NULL,
+	NULL
+};
+
+void
+cmd_copy_mode_exec(unused void *ptr, struct cmd_ctx *ctx)
+{
+	struct window	*w = ctx->session->curw->window;
+
+	if (w->mode == NULL && ctx->flags & CMD_KEY) {
+		w->mode = &window_copy_mode;
+		w->mode->init(w);
+		server_redraw_window_all(w);
+	}
+
+	if (ctx->cmdclient != NULL)
+		server_write_client(ctx->cmdclient, MSG_EXIT, NULL, 0);
+}
