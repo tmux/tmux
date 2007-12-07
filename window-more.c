db6ad3acec3c8afd4c2dd33f33fad2bd544ce9a3
Nicholas Marriott <nicholas.marriott@gmail.com>
2007-12-07 09:26:56 +0000

    Redraw position indicator regardless.

--- window-more.c
+++ window-more.c
@@ -1,4 +1,4 @@
-/* $Id: window-more.c,v 1.8 2007-12-06 21:42:00 nicm Exp $ */
+/* $Id: window-more.c,v 1.9 2007-12-07 09:26:56 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -60,14 +60,15 @@ window_more_vadd(struct window *w, const char *fmt, va_list ap)
 	xvasprintf(&msg, fmt, ap);
 	ARRAY_ADD(&data->list, msg);
 
+	screen_write_start_window(&ctx, w);
 	size = ARRAY_LENGTH(&data->list) - 1;
 	if (size >= data->top && size <= data->top + screen_last_y(s)) {
-		screen_write_start_window(&ctx, w);
 		window_more_write_line(w, &ctx, size - data->top);
 		if (size != data->top)
 			window_more_write_line(w, &ctx, 0);
-		screen_write_stop(&ctx);
-	}
+	} else
+		window_more_write_line(w, &ctx, 0);
+	screen_write_stop(&ctx);
 }
 
 void
