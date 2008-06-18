ee32d19432f3e411b1a426758d74ffc9fa318cc4
Nicholas Marriott <nicholas.marriott@gmail.com>
2008-06-18 16:34:07 +0000

    Scroll status line to show windows off-screen.

--- status.c
+++ status.c
@@ -1,4 +1,4 @@
-/* $Id: status.c,v 1.26 2008-06-15 08:01:54 nicm Exp $ */
+/* $Id: status.c,v 1.27 2008-06-18 16:34:07 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -24,72 +24,174 @@
 
 #include "tmux.h"
 
+size_t	status_width(struct winlink *);
+char   *status_print(struct session *, struct winlink *, u_char *);
+
 /* Draw status for client on the last lines of given context. */
 void
 status_redraw(struct client *c)
 {
 	struct screen_redraw_ctx	ctx;
+	struct session		       *s = c->session;
 	struct winlink		       *wl;
-	char		 		flag, *left, *right;
+	char		 		*left, *right, *text, *ptr;
 	char				lbuf[BUFSIZ], rbuf[BUFSIZ];
-	size_t				llen, rlen;
-	u_char		 		scolour;
-	u_int		 		slines;
+	size_t				llen, rlen, offset, xx, yy;
+	size_t				size, start, width;
+	u_char		 		attr, colr;
 
-	scolour = options_get_colours(&c->session->options, "status-colour");
-	slines = options_get_number(&c->session->options, "status-lines");
-	if (slines == 0 || c->sy <= slines)
+	yy = options_get_number(&s->options, "status-lines");
+	if (c->sy == 0 || yy == 0)
 		return;
 
 	if (clock_gettime(CLOCK_REALTIME, &c->status_ts) != 0)
-		fatal("clock_gettime failed");
+		fatal("clock_gettime failed");	
+	colr = options_get_colours(&s->options, "status-colour");
 
-	left = options_get_string(&c->session->options, "status-left");
+	yy = c->sy - yy;
+	if (yy == 0)
+		goto blank;
+
+	left = options_get_string(&s->options, "status-left");
 	strftime(lbuf, sizeof lbuf, left, localtime(&(c->status_ts.tv_sec)));
-	llen = strlen(lbuf) + 1;
-	right = options_get_string(&c->session->options, "status-right");
+	llen = strlen(lbuf);
+	right = options_get_string(&s->options, "status-right");
 	strftime(rbuf, sizeof rbuf, right, localtime(&(c->status_ts.tv_sec)));
-	rlen = strlen(rbuf) + 1;
+	rlen = strlen(rbuf);
 
+	/*
+	 * Figure out how much space we have for the window list. If there isn't
+	 * enough space, just wimp out.
+	 */
+	xx = 0;
+	if (llen != 0)
+		xx += llen + 1;
+	if (rlen != 0)
+		xx += rlen + 1;
+	if (c->sx == 0 || c->sx <= xx)
+		goto blank;
+	xx = c->sx - xx;
+
+	/* 
+	 * Right. We have xx characters to fill. Find out how much is to go in
+	 * them and the offset of the current window (it must be on screen).
+	 */
+	width = offset = 0;
+	RB_FOREACH(wl, winlinks, &s->windows) {
+		size = status_width(wl) + 1;
+		if (wl == s->curw)
+			offset = width;
+		width += size;
+	}
+	start = 0;
+
+	/* If there is enough space for the total width, all is gravy. */
+	if (width <= xx)
+		goto draw;
+
+	/* Find size of current window text. */
+	size = status_width(s->curw);
+
+	/*
+	 * If the offset is already on screen, we're good to draw from the
+	 * start and just leave off the end.
+	 */
+	if (offset + size < xx) {
+		width = xx - 1;
+		goto draw;
+	}
+
+	/*
+	 * Work out how many characters we need to omit from the start. There
+	 * are xx characters to fill, and offset + size must be the last. So,
+	 * the start character is offset + size - xx.
+	 */
+	start = offset + size - xx;
+	width = xx - 1;
+
+draw:
+ 	/* Begin drawing and move to the starting position. */
 	screen_redraw_start_client(&ctx, c);
-	screen_redraw_move_cursor(&ctx, llen, c->sy - slines);
-	screen_redraw_set_attributes(&ctx, 0, scolour);
+	screen_redraw_set_attributes(&ctx, 0, colr);
+	if (llen != 0) {
+ 		screen_redraw_move_cursor(&ctx, 0, yy);
+		screen_redraw_write_string(&ctx, "%s ", lbuf);
+	} else
+		screen_redraw_move_cursor(&ctx, 0, yy);
 
-	RB_FOREACH(wl, winlinks, &c->session->windows) {
-		flag = ' ';
-		if (wl == c->session->lastw)
-			flag = '-';
-		if (wl == c->session->curw)
-			flag = '*';
-		if (session_alert_has(c->session, wl, WINDOW_ACTIVITY)) {
-			flag = '#';
-			screen_redraw_set_attributes(
-			    &ctx, ATTR_REVERSE, scolour);
+	/* Draw each character in succession. */
+	offset = 0;
+	RB_FOREACH(wl, winlinks, &s->windows) {
+		text = status_print(s, wl, &attr);
+
+		screen_redraw_set_attributes(&ctx, attr, colr);
+		for (ptr = text; *ptr != '\0'; ptr++) { 
+			if (offset >= start && offset <= start + width)
+				ctx.write(ctx.data, TTY_CHARACTER, *ptr);
+			offset++;
 		}
-		if (session_alert_has(c->session, wl, WINDOW_BELL)) {
-			flag = '!';
-			screen_redraw_set_attributes(
-			    &ctx, ATTR_REVERSE, scolour);
+
+		xfree(text);
+
+		if (offset <= start + width) {
+			if (offset >= start) {
+				screen_redraw_set_attributes(&ctx, 0, colr);
+				ctx.write(ctx.data, TTY_CHARACTER, ' ');
+			}
+			offset++;
 		}
-		screen_redraw_write_string(
-		    &ctx, "%d:%s%c", wl->idx, wl->window->name, flag);
-		if (flag == '!' || flag == '#')
-			screen_redraw_set_attributes(&ctx, 0, scolour);
-		screen_redraw_write_string(&ctx, " ");
-		
-		if (ctx.s->cx > c->sx - rlen)
+
+		if (offset >= start + width)
 			break;
 	}
-	while (ctx.s->cx < c->sx - rlen) {
+
+	/* Fill the remaining space if any. */
+	screen_redraw_set_attributes(&ctx, 0, colr);
+ 	while (offset++ < xx)
 		ctx.write(ctx.data, TTY_CHARACTER, ' ');
-		ctx.s->cx++;
+	if (rlen > 0)
+		screen_redraw_write_string(&ctx, " %s", rbuf);
+
+	screen_redraw_stop(&ctx);	
+	return;
+
+blank:
+ 	/* Just draw the whole line as blank. */
+	screen_redraw_start_client(&ctx, c);
+	screen_redraw_set_attributes(&ctx, 0, colr);
+	screen_redraw_move_cursor(&ctx, 0, yy);
+	for (offset = 0; offset < c->sx; offset++) 
+		ctx.write(ctx.data, TTY_CHARACTER, ' ');
+	screen_redraw_stop(&ctx);	
+}
+
+size_t
+status_width(struct winlink *wl)
+{
+	return (xsnprintf(NULL, 0, "%d:%s ", wl->idx, wl->window->name));
+}
+
+char *
+status_print(struct session *s, struct winlink *wl, u_char *attr)
+{
+	char   *text, flag;
+
+	flag = ' ';
+ 	if (wl == s->lastw)
+		flag = '-';
+	if (wl == s->curw)
+		flag = '*';
+
+	*attr = 0;
+	if (session_alert_has(s, wl, WINDOW_ACTIVITY)) {
+		flag = '#';
+		*attr = ATTR_REVERSE;
+	}
+	if (session_alert_has(s, wl, WINDOW_BELL)) {
+		flag = '!';
+		*attr = ATTR_REVERSE;
 	}
 
-	screen_redraw_move_cursor(&ctx, 0, c->sy - slines);
-	screen_redraw_write_string(&ctx, "%s ", lbuf);
-
-	screen_redraw_move_cursor(&ctx, c->sx - rlen, c->sy - slines);
-	screen_redraw_write_string(&ctx, " %s", rbuf);
-
-	screen_redraw_stop(&ctx);
+	xasprintf(&text, "%d:%s%c", wl->idx, wl->window->name, flag);
+	return (text);
 }
