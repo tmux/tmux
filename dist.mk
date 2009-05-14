6099a3399ecf19570ce65bc448ae4bc71c94fcf6
Nicholas Marriott <nicholas.marriott@gmail.com>
2009-05-14 07:48:42 +0000

    Add distribution stuff back in.

---
+++ dist.mk
@@ -0,0 +1,30 @@
+# $Id: dist.mk,v 1.1 2009-05-14 07:48:42 nicm Exp $
+
+DISTDIR= tmux-${VERSION}
+DISTFILES= *.[ch] Makefile GNUmakefile configure tmux.1 \
+	NOTES TODO CHANGES FAQ \
+	`find examples compat -type f -and ! -path '*CVS*'`
+
+dist:          
+		make clean
+		grep '^#FDEBUG=' Makefile
+		grep '^#FDEBUG=' GNUmakefile
+		[ "`(grep '^VERSION' Makefile; grep '^VERSION' GNUmakefile)| \
+		        uniq -u`" = "" ]
+		tar -zc \
+		        -s '/.*/${DISTDIR}\/\0/' \
+		        -f ${DISTDIR}.tar.gz ${DISTFILES}
+
+upload-index.html: update-index.html
+		scp index.html images/*.png \
+		        nicm,tmux@web.sf.net:/home/groups/t/tm/tmux/htdocs
+		rm -f images/small-*
+
+update-index.html:
+		(cd images && \
+		        rm -f small-* && \
+		        for i in *.png; do \
+		        convert "$$i" -resize 200x150 "small-$$i"; \
+		        done \
+		)
+		sed "s/%%VERSION%%/${VERSION}/g" index.html.in >index.html
