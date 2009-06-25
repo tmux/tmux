0af7fa08d2c2933a4b337bd00680737a7c85919e
Nicholas Marriott <nicholas.marriott@gmail.com>
2009-06-25 16:54:21 +0000

    Skip 8 line diffs ($Id$ change only).

--- tools/cmp-cvs.sh
+++ tools/cmp-cvs.sh
@@ -1,5 +1,12 @@
-# $Id: cmp-cvs.sh,v 1.1 2009-06-25 16:49:22 nicm Exp $
+# $Id: cmp-cvs.sh,v 1.2 2009-06-25 16:54:21 nicm Exp $
+
+rm diff.out
+touch diff.out
 
 for i in *.[ch]; do
-    diff -u $i /usr/src/usr.bin/tmux/$i
+    diff -u $i /usr/src/usr.bin/tmux/$i >diff.tmp
+    set -- `wc -l diff.tmp`
+    [ $1 -eq 8 ] && continue
+    echo $i
+    cat diff.tmp >>diff.out
 done
