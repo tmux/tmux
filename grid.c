bd098c273b6a8ac6a0d493194cacd2e231bbeefe
Nicholas Marriott <nicm@openbsd.org>
2009-07-11 20:11:18 +0000

    Limit the history to hlimit not hlimit - 1. This makes a history-limit setting of 0 work as expected.

--- grid.c
+++ grid.c
@@ -172,7 +172,7 @@ grid_scroll_line(struct grid *gd)
 
  	GRID_DEBUG(gd, "");
 
-	if (gd->hsize >= gd->hlimit - 1) {
+	if (gd->hsize >= gd->hlimit) {
 		/* If the limit is hit, free the bottom 10% and shift up. */
 		yy = gd->hlimit / 10;
 		if (yy < 1)
