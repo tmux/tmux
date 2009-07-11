289320a9b19e6be855756e54a545615026b099f8
Nicholas Marriott <nicm@openbsd.org>
2009-07-11 19:09:24 +0000

    Copy was using the real line length which after resize can be larger than the screen width. When built with -DDEBUG, this made the grid bounds checking code kill the server. Restrict copying to the actual width.
    From Kalle Olavi Niemitalo, thanks.

--- window-copy.c
+++ window-copy.c
@@ -522,7 +522,15 @@ window_copy_find_length(struct window_pane *wp, u_int py)
 	const struct grid_cell	*gc;
 	u_int			 px;
 
+	/*
+	 * If the pane has been resized, its grid can contain old overlong
+	 * lines. grid_peek_cell does not allow accessing cells beyond the
+	 * width of the grid, and screen_write_copy treats them as spaces, so
+	 * ignore them here too.
+	 */
 	px = wp->base.grid->size[py];
+	if (px > screen_size_x(&wp->base))
+		px = screen_size_x(&wp->base);
 	while (px > 0) {
 		gc = grid_peek_cell(wp->base.grid, px - 1, py);
 		if (gc->flags & GRID_FLAG_UTF8)
