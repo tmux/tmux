aa3403a3432532984b7057c0f0a34d0c71133c1c
Nicholas Marriott <nicm@openbsd.org>
2009-07-15 08:05:56 +0000

    Add a -k flag to unlink-window which makes it behave the same as the old kill-window - if a window is linked into only one session it unlinked and destroyed.

--- cmd-unlink-window.c
+++ cmd-unlink-window.c
@@ -28,8 +28,8 @@ int	cmd_unlink_window_exec(struct cmd *, struct cmd_ctx *);
 
 const struct cmd_entry cmd_unlink_window_entry = {
 	"unlink-window", "unlinkw",
-	CMD_TARGET_WINDOW_USAGE,
-	0, 0,
+	"[-k] " CMD_TARGET_WINDOW_USAGE,
+	0, CMD_CHFLAG('k'),
 	cmd_target_init,
 	cmd_target_parse,
 	cmd_unlink_window_exec,
@@ -52,7 +52,7 @@ cmd_unlink_window_exec(struct cmd *self, struct cmd_ctx *ctx)
 	if ((wl = cmd_find_window(ctx, data->target, &s)) == NULL)
 		return (-1);
 
-	if (wl->window->references == 1) {
+	if (!(data->chflags & CMD_CHFLAG('k')) && wl->window->references == 1) {
 		ctx->error(ctx, "window is only linked to one session");
 		return (-1);
 	}
