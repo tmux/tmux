1c58b941412ffb24ee46d6852802a6b453a3c9a3
Nicholas Marriott <nicm@openbsd.org>
2009-08-18 11:53:03 +0000

    Use the full screen width when printing output rather than one less.

--- window-more.c
+++ window-more.c
@@ -188,7 +188,7 @@ window_more_write_line(
 	if (data->top + py  < ARRAY_LENGTH(&data->list)) {
 		msg = ARRAY_ITEM(&data->list, data->top + py);
 		screen_write_nputs(
-		    ctx, screen_size_x(s) - 1 - size, &gc, utf8flag, "%s", msg);
+		    ctx, screen_size_x(s) - size, &gc, utf8flag, "%s", msg);
 	}
 	while (s->cx < screen_size_x(s) - size)
 		screen_write_putc(ctx, &gc, ' ');
