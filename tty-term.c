2c9d4dfdd4568f40ab1f3e7a20918d1f17ea0482
Nicholas Marriott <nicholas.marriott@gmail.com>
2009-09-22 19:20:08 +0000

    Tweak tparm() to make Solaris' backward userland happy and remove the -I mess in configure which tried to deal with the fallout from their horrible package management.

--- tty-term.c
+++ tty-term.c
@@ -1,4 +1,4 @@
-/* $Id: tty-term.c,v 1.30 2009-08-23 11:50:39 nicm Exp $ */
+/* $Id: tty-term.c,v 1.31 2009-09-22 19:20:08 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -417,13 +417,13 @@ tty_term_string(struct tty_term *term, enum tty_code_code code)
 const char *
 tty_term_string1(struct tty_term *term, enum tty_code_code code, int a)
 {
-	return (tparm((char *) tty_term_string(term, code), a));
+	return (tparm((char *) tty_term_string(term, code), a, 0, 0, 0, 0, 0, 0, 0, 0));
 }
 
 const char *
 tty_term_string2(struct tty_term *term, enum tty_code_code code, int a, int b)
 {
-	return (tparm((char *) tty_term_string(term, code), a, b));
+	return (tparm((char *) tty_term_string(term, code), a, b, 0, 0, 0, 0, 0, 0, 0));
 }
 
 int
