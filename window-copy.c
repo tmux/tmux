a505ca69d9417257d2fe0c47376a0e21833802c2
Tiago Cunha <tcunha@gmx.com>
2009-10-15 01:52:47 +0000

    Sync OpenBSD patchset 407:
    Add mode keys to move the cursor to the top, middle and bottom of the screen.
    H/M/L in vi mode and M-R/M-r in emacs (bottom of screen not bound in emacs).

--- window-copy.c
+++ window-copy.c
@@ -1,4 +1,4 @@
-/* $Id: window-copy.c,v 1.88 2009-10-12 00:18:19 tcunha Exp $ */
+/* $Id: window-copy.c,v 1.89 2009-10-15 01:52:47 tcunha Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -275,6 +275,24 @@ window_copy_key(struct window_pane *wp, struct client *c, int key)
 			data->oy = 0;
 		else
 			data->oy -= n;
+		window_copy_update_selection(wp);
+		window_copy_redraw_screen(wp);
+		break;
+	case MODEKEYCOPY_TOPLINE:
+		data->cx = 0;
+		data->cy = 0;
+		window_copy_update_selection(wp);
+		window_copy_redraw_screen(wp);
+		break;
+	case MODEKEYCOPY_MIDDLELINE:
+		data->cx = 0;
+		data->cy = (screen_size_y(s) - 1) / 2;
+		window_copy_update_selection(wp);
+		window_copy_redraw_screen(wp);
+		break;
+	case MODEKEYCOPY_BOTTOMLINE:
+		data->cx = 0;
+		data->cy = screen_size_y(s) - 1;
 		window_copy_update_selection(wp);
 		window_copy_redraw_screen(wp);
 		break;
