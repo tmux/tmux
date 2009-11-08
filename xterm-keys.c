8fe9ecae05c0c2c0bf5c065d43217eb32bd9a629
Tiago Cunha <tcunha@gmx.com>
2009-11-08 23:33:57 +0000

    Sync OpenBSD patchset 515:
    Old xterm F1-F4 are \033O_P not \033[O_P.

--- xterm-keys.c
+++ xterm-keys.c
@@ -1,4 +1,4 @@
-/* $Id: xterm-keys.c,v 1.2 2009-10-28 23:05:43 tcunha Exp $ */
+/* $Id: xterm-keys.c,v 1.3 2009-11-08 23:33:57 tcunha Exp $ */
 
 /*
  * Copyright (c) 2009 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -50,13 +50,13 @@ struct xterm_keys_entry {
 
 struct xterm_keys_entry xterm_keys_table[] = {
 	{ KEYC_F1,	"\033[1;_P" },
-	{ KEYC_F1,	"\033[O_P" },
+	{ KEYC_F1,	"\033O_P" },
 	{ KEYC_F2,	"\033[1;_Q" },
-	{ KEYC_F2,	"\033[O_Q" },
+	{ KEYC_F2,	"\033O_Q" },
 	{ KEYC_F3,	"\033[1;_R" },
-	{ KEYC_F3,	"\033[O_R" },
+	{ KEYC_F3,	"\033O_R" },
 	{ KEYC_F4,	"\033[1;_S" },
-	{ KEYC_F4,	"\033[O_S" },
+	{ KEYC_F4,	"\033O_S" },
 	{ KEYC_F5,	"\033[15;_~" },
 	{ KEYC_F6,	"\033[17;_~" },
 	{ KEYC_F7,	"\033[18;_~" },
