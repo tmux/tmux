1415eb3dd249e7730b151a5cfd61c47a712a522f
Nicholas Marriott <nicm@openbsd.org>
2009-11-13 14:47:31 +0000

    Use winlink_remove() to remove old winlinks when synchronizing grouped sessions rather than doing it manually and not adjusted the reference count. Fixes crash seen by Dan Harnett.

--- session.c
+++ session.c
@@ -589,7 +589,6 @@ session_group_synchronize1(struct session *target, struct session *s)
 	/* Then free the old winlinks list. */
 	while (!RB_EMPTY(&old_windows)) {
 		wl = RB_ROOT(&old_windows);
-		RB_REMOVE(winlinks, &old_windows, wl);
-		xfree(wl);
+		winlink_remove(&old_windows, wl);
 	}
 }
