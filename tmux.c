8512811535658a49ed738904233c9289e152a042
Tiago Cunha <tcunha@gmx.com>
2009-11-19 22:31:27 +0000

    Sync OpenBSD patchset 551:
    Revert to xterm-keys off by default. It was on as an experiment to see if the
    option could be removed, but it affects vi, so we have to keep the option, and
    a conservative default is better.

--- tmux.c
+++ tmux.c
@@ -1,4 +1,4 @@
-/* $Id: tmux.c,v 1.189 2009-11-19 22:20:04 tcunha Exp $ */
+/* $Id: tmux.c,v 1.190 2009-11-19 22:31:27 tcunha Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -394,7 +394,7 @@ main(int argc, char **argv)
 	options_set_number(wo, "window-status-current-bg", 8);
 	options_set_number(wo, "window-status-current-fg", 8);
 	options_set_number(wo, "window-status-fg", 8);
-	options_set_number(wo, "xterm-keys", 1);
+	options_set_number(wo, "xterm-keys", 0);
  	options_set_number(wo, "remain-on-exit", 0);
 	options_set_number(wo, "synchronize-panes", 0);
 
