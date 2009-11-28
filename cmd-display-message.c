5f366e6d54c4825eb760424f86388e8813bb6189
Tiago Cunha <tcunha@gmx.com>
2009-11-28 14:39:53 +0000

    Sync OpenBSD patchset 561:
    Add a -p flag to display-message to print the output rather than displaying in
    the status line, this allows things like "display -p '#W'" to find the current
    window index.

--- cmd-display-message.c
+++ cmd-display-message.c
@@ -1,4 +1,4 @@
-/* $Id: cmd-display-message.c,v 1.6 2009-11-19 22:35:10 tcunha Exp $ */
+/* $Id: cmd-display-message.c,v 1.7 2009-11-28 14:39:53 tcunha Exp $ */
 
 /*
  * Copyright (c) 2009 Tiago Cunha <me@tiagocunha.org>
@@ -30,8 +30,8 @@ int	cmd_display_message_exec(struct cmd *, struct cmd_ctx *);
 
 const struct cmd_entry cmd_display_message_entry = {
 	"display-message", "display",
-	CMD_TARGET_CLIENT_USAGE " [message]",
-	CMD_ARG01, "",
+	"[-p] " CMD_TARGET_CLIENT_USAGE " [message]",
+	CMD_ARG01, "p",
 	cmd_target_init,
 	cmd_target_parse,
 	cmd_display_message_exec,
@@ -56,7 +56,10 @@ cmd_display_message_exec(struct cmd *self, struct cmd_ctx *ctx)
 		template = data->arg;
 
 	msg = status_replace(c, NULL, template, time(NULL), 0);
-	status_message_set(c, "%s", msg);
+	if (cmd_check_flag(data->chflags, 'p'))
+		ctx->print(ctx, "%s", msg);
+	else
+		status_message_set(c, "%s", msg);
 	xfree(msg);
 
 	return (0);
