9ffe549ab199b6af8cd5e7b8ac3611fdba1d6b62
Nicholas Marriott <nicm@openbsd.org>
2010-02-01 23:06:24 +0000

    If redrawing line 0 of the screen onto the tty, there can't be a wrap flag on the previous line, so move the cursor. Fixes status line redraw issues when resizing in choose mode and hopefully at other times as well.

--- tty.c
+++ tty.c
@@ -484,7 +484,7 @@ tty_draw_line(struct tty *tty, struct screen *s, u_int py, u_int ox, u_int oy)
 	gl = NULL;
 	if (py != 0)
 		gl = &s->grid->linedata[s->grid->hsize + py - 1];
-	if (oy + py == 0 || (gl != NULL && !(gl->flags & GRID_LINE_WRAPPED)) ||
+	if (oy + py == 0 || gl == NULL || !(gl->flags & GRID_LINE_WRAPPED) ||
 	    tty->cx < tty->sx || ox != 0 ||
 	    (oy + py != tty->cy + 1 && tty->cy != s->rlower + oy))
 		tty_cursor(tty, ox, oy + py);
