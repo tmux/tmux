608eef731a1e6973cff6e109aea31fabfdd8baaf
Nicholas Marriott <nicm@openbsd.org>
2010-05-25 19:47:30 +0000

    Fix an out-of-date comment.

--- cfg.c
+++ cfg.c
@@ -69,7 +69,7 @@ cfg_add_cause(struct causelist *causes, const char *fmt, ...)
 
 /*
  * Load configuration file. Returns -1 for an error with a list of messages in
- * causes. Note that causes and ncauses must be initialised by the caller!
+ * causes. Note that causes must be initialised by the caller!
  */
 int
 load_cfg(const char *path, struct cmd_ctx *ctxin, struct causelist *causes)
