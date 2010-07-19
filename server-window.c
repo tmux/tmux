2b80ede9631b8236aa213c730cd6d91805e761f9
Nicholas Marriott <nicm@openbsd.org>
2010-07-19 21:13:03 +0000

    Don't return if in the current window since we may want to report a bell (if bell-action any/current), just clear the flag so the status line doesn't show the bell.

--- server-window.c
+++ server-window.c
@@ -109,10 +109,8 @@ server_window_check_bell(struct session *s, struct winlink *wl)
 
 	if (!(w->flags & WINDOW_BELL) || wl->flags & WINLINK_BELL)
 		return (0);
-	if (s->curw == wl)
-		return (0);
-
-	wl->flags |= WINLINK_BELL;
+	if (s->curw != wl)
+		wl->flags |= WINLINK_BELL;
 
 	action = options_get_number(&s->options, "bell-action");
 	switch (action) {
