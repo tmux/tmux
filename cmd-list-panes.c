933dc48de356e387c0c027e654e1ae5f2f24c711
Nicholas Marriott <nicm@openbsd.org>
2010-08-11 07:27:50 +0000

    Show which pane is active in the list-panes output, suggested by Dominik Honnef.

--- cmd-list-panes.c
+++ cmd-list-panes.c
@@ -65,8 +65,9 @@ cmd_list_panes_exec(struct cmd *self, struct cmd_ctx *ctx)
 		}
 		size += gd->hsize * sizeof *gd->linedata;
 
-		ctx->print(ctx, "%u: [%ux%u] [history %u/%u, %llu bytes]",
-		    n, wp->sx, wp->sy, gd->hsize, gd->hlimit, size);
+		ctx->print(ctx, "%u: [%ux%u] [history %u/%u, %llu bytes]%s",
+		    n, wp->sx, wp->sy, gd->hsize, gd->hlimit, size,
+		    wp == wp->window->active ? " (active)" : "");
 		n++;
 	}
 
