fb527c1bad96b77bfae7b0b6bf3540dad5cd89e2
Nicholas Marriott <nicm@openbsd.org>
2011-05-18 08:04:47 +0000

    Use button mouse mode not any for copy mode, fixes issues with putty. From Ailin Nemui.

--- window-copy.c
+++ window-copy.c
@@ -790,7 +790,7 @@ window_copy_mouse(
 	 * If already reading motion, move the cursor while buttons are still
 	 * pressed, or stop the selection on their release.
 	 */
-	if (s->mode & MODE_MOUSE_ANY) {
+	if (s->mode & MODE_MOUSE_BUTTON) {
 		if ((m->b & MOUSE_BUTTON) != MOUSE_UP) {
 			window_copy_update_cursor(wp, m->x, m->y);
 			if (window_copy_update_selection(wp))
@@ -803,7 +803,7 @@ window_copy_mouse(
 	/* Otherwise if other buttons pressed, start selection and motion. */
 	if ((m->b & MOUSE_BUTTON) != MOUSE_UP) {
 		s->mode &= ~MODE_MOUSE_STANDARD;
-		s->mode |= MODE_MOUSE_ANY;
+		s->mode |= MODE_MOUSE_BUTTON;
 
 		window_copy_update_cursor(wp, m->x, m->y);
 		window_copy_start_selection(wp);
@@ -813,7 +813,7 @@ window_copy_mouse(
 	return;
 
 reset_mode:
-	s->mode &= ~MODE_MOUSE_ANY;
+	s->mode &= ~MODE_MOUSE_BUTTON;
 	s->mode |= MODE_MOUSE_STANDARD;
 	if (sess != NULL) {
 		window_copy_copy_selection(wp);
