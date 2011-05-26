34e5ec18074b05f8ffc95bdefe7f7037ff046ecf
Nicholas Marriott <nicm@openbsd.org>
2011-05-26 07:08:48 +0000

    Trim another useless if statement, from Ailin Nemui.

--- tty.c
+++ tty.c
@@ -248,7 +248,7 @@ tty_stop_tty(struct tty *tty)
 	if (tty_term_has(tty->term, TTYC_CS1) && tty->cstyle != 0) {
 		if (tty_term_has(tty->term, TTYC_CSR1))
 			tty_raw(tty, tty_term_string(tty->term, TTYC_CSR1));
-		else if (tty_term_has(tty->term, TTYC_CS1))
+		else
 			tty_raw(tty, tty_term_string1(tty->term, TTYC_CS1, 0));
 	}
 	tty_raw(tty, tty_term_string(tty->term, TTYC_CR));
