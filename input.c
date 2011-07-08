dc2c174496532a915120c6fd12b26df742a3ee8e
Tiago Cunha <tcunha@gmx.com>
2011-07-08 08:40:35 +0000

    Sync OpenBSD patchset 933:
    Fix a couple of comments.

--- input.c
+++ input.c
@@ -1,4 +1,4 @@
-/* $Id: input.c,v 1.119 2011-05-22 16:26:09 tcunha Exp $ */
+/* $Id$ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -555,7 +555,7 @@ const struct input_transition input_state_dcs_escape_table[] = {
 	{ -1, -1, NULL, NULL }
 };
 
-/* device_ignore state table. */
+/* dcs_ignore state table. */
 const struct input_transition input_state_dcs_ignore_table[] = {
 	INPUT_STATE_ANYWHERE,
 
@@ -791,7 +791,7 @@ input_split(struct input_ctx *ictx)
 	return (0);
 }
 
-/* Get an argument or return default value..*/
+/* Get an argument or return default value. */
 int
 input_get(struct input_ctx *ictx, u_int validx, int minval, int defval)
 {
