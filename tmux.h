a34a72d2d915d9a1f24d1643132352ce3c07f82a
Tiago Cunha <tcunha@gmx.com>
2012-01-23 12:24:00 +0000

    Sync OpenBSD patchset 1011:
    Remove unused backoff code that doesn't do any good.

--- tmux.h
+++ tmux.h
@@ -1164,8 +1164,7 @@ struct client {
 #define CLIENT_DEAD 0x200
 #define CLIENT_BORDERS 0x400
 #define CLIENT_READONLY 0x800
-#define CLIENT_BACKOFF 0x1000
-#define CLIENT_REDRAWWINDOW 0x2000
+#define CLIENT_REDRAWWINDOW 0x1000
 	int		 flags;
 
 	struct event	 identify_timer;
