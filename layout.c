711d2205ddc4a81c26ab8eb7d647c52492540b03
Nicholas Marriott <nicm@openbsd.org>
2012-04-01 21:07:35 +0000

    Fix some indentation.

--- layout.c
+++ layout.c
@@ -498,18 +498,18 @@ layout_resize_pane_mouse(struct client *c, struct mouse_event *mouse)
 
 	pane_border = 0;
 	if ((c->last_mouse.b & MOUSE_BUTTON) != MOUSE_UP &&
-		(c->last_mouse.b & MOUSE_RESIZE_PANE)) {
+	    (c->last_mouse.b & MOUSE_RESIZE_PANE)) {
 		TAILQ_FOREACH(wp, &w->panes, entry) {
 			if (wp->xoff + wp->sx == c->last_mouse.x &&
-				wp->yoff <= 1 + c->last_mouse.y &&
-				wp->yoff + wp->sy >= c->last_mouse.y) {
+			    wp->yoff <= 1 + c->last_mouse.y &&
+			    wp->yoff + wp->sy >= c->last_mouse.y) {
 				layout_resize_pane(wp, LAYOUT_LEFTRIGHT,
 				    mouse->x - c->last_mouse.x);
 				pane_border = 1;
 			}
 			if (wp->yoff + wp->sy == c->last_mouse.y &&
-				wp->xoff <= 1 + c->last_mouse.x &&
-				wp->xoff + wp->sx >= c->last_mouse.x) {
+			    wp->xoff <= 1 + c->last_mouse.x &&
+			    wp->xoff + wp->sx >= c->last_mouse.x) {
 				layout_resize_pane(wp, LAYOUT_TOPBOTTOM,
 				    mouse->y - c->last_mouse.y);
 				pane_border = 1;
@@ -518,14 +518,14 @@ layout_resize_pane_mouse(struct client *c, struct mouse_event *mouse)
 		if (pane_border)
 			server_redraw_window(w);
 	} else if (mouse->b != MOUSE_UP &&
-			   mouse->b == (mouse->b & MOUSE_BUTTON)) {
+	    mouse->b == (mouse->b & MOUSE_BUTTON)) {
 		TAILQ_FOREACH(wp, &w->panes, entry) {
 			if ((wp->xoff + wp->sx == mouse->x &&
-				 wp->yoff <= 1 + mouse->y &&
-				 wp->yoff + wp->sy >= mouse->y) ||
-				(wp->yoff + wp->sy == mouse->y &&
-				 wp->xoff <= 1 + mouse->x &&
-				 wp->xoff + wp->sx >= mouse->x)) {
+			    wp->yoff <= 1 + mouse->y &&
+			    wp->yoff + wp->sy >= mouse->y) ||
+			    (wp->yoff + wp->sy == mouse->y &&
+			    wp->xoff <= 1 + mouse->x &&
+			    wp->xoff + wp->sx >= mouse->x)) {
 				pane_border = 1;
 			}
 		}
