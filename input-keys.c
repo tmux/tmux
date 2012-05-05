218b692221b9dd675275ec353364910dfc072151
Nicholas Marriott <nicm@openbsd.org>
2012-05-05 18:48:31 +0000

    Only enter copy mode on scroll up, from Ailin Nemui.

--- input-keys.c
+++ input-keys.c
@@ -223,7 +223,8 @@ input_mouse(struct window_pane *wp, struct mouse_event *m)
 		return;
 	}
 
-	if (options_get_number(&wp->window->options, "mode-mouse") == 1) {
+	if ((m->b & 3) != 1 &&
+	    options_get_number(&wp->window->options, "mode-mouse") == 1) {
 		if (window_pane_set_mode(wp, &window_copy_mode) == 0) {
 			window_copy_init_from_pane(wp);
 			if (wp->mode->mouse != NULL)
