3604b31b6f1deb996102f5e8767818b6dcf2d582
Tiago Cunha <tcunha@gmx.com>
2012-05-22 20:50:03 +0000

    Sync OpenBSD patchset 1112:
    Use -t for move-window with -r rather than dying. Reported by Ben
    Boeckel and Thomas Adam.

--- cmd-move-window.c
+++ cmd-move-window.c
@@ -47,11 +47,10 @@ cmd_move_window_exec(struct cmd *self, struct cmd_ctx *ctx)
 	char		*cause;
 	int		 idx, kflag, dflag;
 
-	if ((s = ctx->curclient->session) == NULL)
-		return (-1);
+	if (args_has(args, 'r')) {
+		if ((s = cmd_find_session(ctx, args_get(args, 't'), 0)) == NULL)
+			return (-1);
 
-	if (args_has(args, 'r'))
-	{
 		session_renumber_windows(s);
 		recalculate_sizes();
 
