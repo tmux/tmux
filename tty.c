44dccf7ea27edb938b732f5b9eb1d59ccb9a11fd
Nicholas Marriott <nicm@openbsd.org>
2012-09-29 06:57:56 +0000

    Do not test client flags against TTY_FREEZE bit, reported by Tom Ryder.

--- tty.c
+++ tty.c
@@ -663,7 +663,9 @@ tty_write(
 		c = ARRAY_ITEM(&clients, i);
 		if (c == NULL || c->session == NULL || c->tty.term == NULL)
 			continue;
-		if (c->flags & (CLIENT_SUSPENDED|TTY_FREEZE))
+		if (c->flags & CLIENT_SUSPENDED)
+			continue;
+		if (c->tty.flags & TTY_FREEZE)
 			continue;
 		if (c->session->curw->window != wp->window)
 			continue;
