fe00607816308953209cb85ab92a586c1f344cde
Thomas Adam <thomas@xteddy.org>
2013-01-31 00:14:59 +0000

    Only set AUTO{MAKE,CONF}_VERSION on OpenBSD
    OpenBSD requires the presence of AUTOMAKE_VERSION and AUTOCONF_VERSION for
    bootstrapping purposes.  Setting these on any other system requires that
    explicit version to be used, rather than what might already be installed.
    
    Therefore, only do this when the platform is OpenBSD and ignore everything
    else.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # $Id$
 
-[ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.10
-[ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.65
+if [ "x$(uname)" = "xOpenBSD" ]; then
+	[ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.10
+	[ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.65
+fi
 
 die()
 {
