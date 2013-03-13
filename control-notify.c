4d38b6d1fab398b0af6d22281d0d4b35448524f7
Nicholas Marriott <nicholas.marriott@gmail.com>
2013-03-13 07:28:12 +0000

    Include prefix on ids, from George Nachman.

--- control-notify.c
+++ control-notify.c
@@ -45,7 +45,7 @@ control_notify_input(struct client *c, struct window_pane *wp,
 	 */
 	if (winlink_find_by_window(&c->session->windows, wp->window) != NULL) {
 		message = evbuffer_new();
-		evbuffer_add_printf(message, "%%output %u ", wp->id);
+		evbuffer_add_printf(message, "%%output %%%u ", wp->id);
 		for (i = 0; i < len; i++) {
 			if (buf[i] < ' ' || buf[i] == '\\')
 			    evbuffer_add_printf(message, "\\%03o", buf[i]);
@@ -108,7 +108,7 @@ control_notify_window_unlinked(unused struct session *s, struct window *w)
 			continue;
 		cs = c->session;
 
-		control_write(c, "%%window-close %u", w->id);
+		control_write(c, "%%window-close @%u", w->id);
 	}
 }
 
@@ -126,9 +126,9 @@ control_notify_window_linked(unused struct session *s, struct window *w)
 		cs = c->session;
 
 		if (winlink_find_by_window_id(&cs->windows, w->id) != NULL)
-			control_write(c, "%%window-add %u", w->id);
+			control_write(c, "%%window-add @%u", w->id);
 		else
-			control_write(c, "%%unlinked-window-add %u", w->id);
+			control_write(c, "%%unlinked-window-add @%u", w->id);
 	}
 }
 
@@ -145,7 +145,7 @@ control_notify_window_renamed(struct window *w)
 			continue;
 		s = c->session;
 
-		control_write(c, "%%window-renamed %u %s", w->id, w->name);
+		control_write(c, "%%window-renamed @%u %s", w->id, w->name);
 	}
 }
 
@@ -158,7 +158,7 @@ control_notify_attached_session_changed(struct client *c)
 		return;
 	s = c->session;
 
-	control_write(c, "%%session-changed %u %s", s->id, s->name);
+	control_write(c, "%%session-changed $%u %s", s->id, s->name);
 }
 
 void
@@ -169,10 +169,10 @@ control_notify_session_renamed(struct session *s)
 
 	for (i = 0; i < ARRAY_LENGTH(&clients); i++) {
 		c = ARRAY_ITEM(&clients, i);
-		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c) || c->session != s)
+		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c))
 			continue;
 
-		control_write(c, "%%session-renamed %s", s->name);
+		control_write(c, "%%session-renamed $%u %s", s->id, s->name);
 	}
 }
 
@@ -184,7 +184,7 @@ control_notify_session_created(unused struct session *s)
 
 	for (i = 0; i < ARRAY_LENGTH(&clients); i++) {
 		c = ARRAY_ITEM(&clients, i);
-		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c) || c->session == NULL)
+		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c))
 			continue;
 
 		control_write(c, "%%sessions-changed");
@@ -199,7 +199,7 @@ control_notify_session_close(unused struct session *s)
 
 	for (i = 0; i < ARRAY_LENGTH(&clients); i++) {
 		c = ARRAY_ITEM(&clients, i);
-		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c) || c->session == NULL)
+		if (!CONTROL_SHOULD_NOTIFY_CLIENT(c))
 			continue;
 
 		control_write(c, "%%sessions-changed");
