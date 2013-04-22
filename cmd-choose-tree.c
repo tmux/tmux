04f54ab38f9a5f32e6fb89b57380cc7150c08006
Nicholas Marriott <nicm@openbsd.org>
2013-04-22 16:34:53 +0000

    Get session of -t window rather than client's window.

--- cmd-choose-tree.c
+++ cmd-choose-tree.c
@@ -89,10 +89,7 @@ cmd_choose_tree_exec(struct cmd *self, struct cmd_q *cmdq)
 		return (CMD_RETURN_ERROR);
 	}
 
-	if ((s = c->session) == NULL)
-		return (CMD_RETURN_ERROR);
-
-	if ((wl = cmd_find_window(cmdq, args_get(args, 't'), NULL)) == NULL)
+	if ((wl = cmd_find_window(cmdq, args_get(args, 't'), &s)) == NULL)
 		return (CMD_RETURN_ERROR);
 
 	if (window_pane_set_mode(wl->window->active, &window_choose_mode) != 0)
