828145456c4c1ab8340c2a38ee7e66333cff1d27
Nicholas Marriott <nicholas.marriott@gmail.com>
2013-06-25 09:48:09 +0100

    Don't set TMUX_CONF in tmux.h.

--- tmux.h
+++ tmux.h
@@ -39,9 +39,6 @@
 extern char    *__progname;
 extern char   **environ;
 
-/* Default global configuration file. */
-#define TMUX_CONF "/etc/tmux.conf"
-
 /* Default prompt history length. */
 #define PROMPT_HISTORY 100
 
