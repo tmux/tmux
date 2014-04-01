d9960b2d4d4cb6697086d32b24e9e6cbc74df483
deraadt <deraadt>
2014-04-01 05:50:30 +0000

    missed commit matching log.c

--- tmux.h
+++ tmux.h
@@ -2331,7 +2331,7 @@ u_int	utf8_split2(u_int, u_char *);
 char   *get_proc_name(int, char *);
 
 /* log.c */
-void		 log_open(int, const char *);
+void		 log_open(const char *);
 void		 log_close(void);
 void printflike1 log_debug(const char *, ...);
 void printflike1 log_debug2(const char *, ...);
