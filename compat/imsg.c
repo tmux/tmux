f199fb6a2b53c99fb92de3db5a7e03ac73fb0db4
Nicholas Marriott <nicholas.marriott@gmail.com>
2015-10-15 09:24:25 +0100

    Fix available_fds when there is no AF_INET, reported by Mathieu Arnold.

--- compat/imsg.c
+++ compat/imsg.c
@@ -51,8 +51,12 @@ available_fds(unsigned int n)
 	for (i = 0; i < n; i++) {
 		fds[i] = -1;
 		if ((fds[i] = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-			ret = 1;
-			break;
+			if (errno == EAFNOSUPPORT || errno == EPROTONOSUPPORT)
+				fds[i] = socket(AF_INET6, SOCK_DGRAM, 0);
+			if (fds[i] < 0) {
+				ret = 1;
+				break;
+			}
 		}
 	}
 
