ca29dc9abc5c90da1a56d06ecb692b35a3766bc0
Nicholas Marriott <nicholas.marriott@gmail.com>
2016-01-19 18:07:25 +0000

    Update my email address.

--- compat/forkpty-sunos.c
+++ compat/forkpty-sunos.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Nicholas Marriott <nicm@users.sourceforge.net>
+ * Copyright (c) 2008 Nicholas Marriott <nicholas.marriott@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
--- compat/setenv.c
+++ compat/setenv.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2010 Dagobert Michelsen
- * Copyright (c) 2010 Nicholas Marriott <nicm@users.sourceforge.net>
+ * Copyright (c) 2010 Nicholas Marriott <nicholas.marriott@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
