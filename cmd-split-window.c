703ae7fdccd75c45d1c186686fab1dc66e29c4f9
Stephen Kent <smkent@smkent.net>
2016-08-14 12:57:44 -0700

    Add full-size window split feature
    This adds a new argument (-f) to the split-window command which causes
    the new pane to be created spanning the full window height (with -h) or
    full window width (with -v) instead of splitting the active pane.
    
    When creating a new full-size split, all existing panes are resized
    proportionally to fit within the reduced window space. The command fails
    if there is not sufficient space available in the window.
    
    To illustrate, split-window -h on the following layout (with pane 1
    active) produces:
    
        +---------+      +----+----+
        | 1       |      | 1  | 3  |
        +---------+  ->  +----+----+
        | 2       |      | 2       |
        +---------+      +---------+
    
    With this feature, split-window -h -f on the same layout produces:
    
        +---------+      +----+----+
        | 1       |      | 1  | 3  |
        +---------+  ->  +----+    |
        | 2       |      | 2  |    |
        +---------+      +---------+

--- cmd-split-window.c
+++ cmd-split-window.c
@@ -38,8 +38,8 @@ const struct cmd_entry cmd_split_window_entry = {
 	.name = "split-window",
 	.alias = "splitw",
 
-	.args = { "bc:dF:l:hp:Pt:v", 0, -1 },
-	.usage = "[-bdhvP] [-c start-directory] [-F format] "
+	.args = { "bc:dfF:l:hp:Pt:v", 0, -1 },
+	.usage = "[-bdfhvP] [-c start-directory] [-F format] "
 		 "[-p percentage|-l size] " CMD_TARGET_PANE_USAGE " [command]",
 
 	.tflag = CMD_PANE,
@@ -130,7 +130,8 @@ cmd_split_window_exec(struct cmd *self, struct cmd_q *cmdq)
 	if (*shell == '\0' || areshell(shell))
 		shell = _PATH_BSHELL;
 
-	lc = layout_split_pane(wp, type, size, args_has(args, 'b'));
+	lc = layout_split_pane(wp, type, size, args_has(args, 'b'),
+	    args_has(args, 'f'));
 	if (lc == NULL) {
 		cause = xstrdup("pane too small");
 		goto error;
