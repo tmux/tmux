5bb3fa97db1e5b1b0505b479696fd056344e79b6
Joshua Rubin <jrubin@zvelo.com>
2016-08-30 15:44:50 -0600

    add copyright to charwidth header

--- compat/charwidth.h
+++ compat/charwidth.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 Joshua Rubin <joshua@rubixconsulting.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef _CHARWIDTH_H_
 #define _CHARWIDTH_H_
 
@@ -46,4 +62,5 @@ wctomb(char *s, wchar_t wchar)
 
 	return utf8proc_encode_char(wchar, s);
 }
+
 #endif
