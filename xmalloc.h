3cf19d6dd0900dd5bbd78594af308ee88b109756
nicm <nicm>
2016-11-17 10:06:08 +0000

    Key running commands for #() by the unexpanded command, and run them again if the expanded form changes (otherwise at most once per second as usual). Fixes issues reported by Gregory Pakosz.

--- xmalloc.h
+++ xmalloc.h
@@ -24,6 +24,7 @@ void	*xcalloc(size_t, size_t);
 void	*xrealloc(void *, size_t);
 void	*xreallocarray(void *, size_t, size_t);
 char	*xstrdup(const char *);
+char	*xstrndup(const char *, size_t);
 int	 xasprintf(char **, const char *, ...)
 		__attribute__((__format__ (printf, 2, 3)))
 		__attribute__((__nonnull__ (2)));
