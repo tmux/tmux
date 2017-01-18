649c0d8e91e799981f4e41d4268a3470512c8412
nicm <nicm>
2017-01-18 08:43:21 +0000

    Do not reset idx, it has just been set.

--- options.c
+++ options.c
@@ -403,7 +403,6 @@ options_match(const char *s, int *idx, int* ambiguous)
 	name = options_parse(s, idx);
 	namelen = strlen(name);
 
-	*idx = -1;
 	if (*name == '@') {
 		*ambiguous = 0;
 		return (xstrdup(name));
