b98b4e23a37ea39bdc6080831935873d8efa1126
Nicholas Marriott <nicholas.marriott@gmail.com>
2017-01-18 17:35:41 +0000

    Put version in log in portable tmux.

--- proc.c
+++ proc.c
@@ -195,8 +195,8 @@ proc_start(const char *name, struct event_base *base, int forkflag,
 	if (uname(&u) < 0)
 		memset(&u, 0, sizeof u);
 
-	log_debug("%s started (%ld): socket %s, protocol %d", name,
-	    (long)getpid(), socket_path, PROTOCOL_VERSION);
+	log_debug("%s started (%ld): version %s, socket %s, protocol %d", name,
+	    (long)getpid(), VERSION, socket_path, PROTOCOL_VERSION);
 	log_debug("on %s %s %s; libevent %s (%s)", u.sysname, u.release,
 	    u.version, event_get_version(), event_get_method());
 
