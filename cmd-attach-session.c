d2bba756cd9baa9f4eed3a624a161f8292cef6ac
Amos Bird <amosbird@gmail.com>
2017-02-04 13:01:05 +0800

    reverting key table changes.
    Without clearing key tables for session switch, some configuration may lead to
    undesired behavior after switching -- prefix indicator is on while not behaving
    like prefix has been pressed, and one key event will be consumed silently.

--- cmd-attach-session.c
+++ cmd-attach-session.c
@@ -98,6 +98,7 @@ cmd_attach_session(struct cmdq_item *item, int dflag, int rflag,
 			environ_update(s->options, c->environ, s->environ);
 
 		c->session = s;
+		server_client_set_key_table(c, NULL);
 		status_timer_start(c);
 		notify_client("client-session-changed", c);
 		session_update_activity(s, NULL);
