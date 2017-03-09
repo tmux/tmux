6a724079fbda9a76b4155be8b66a23d43365cde4
Dagobert Michelsen <dam@opencsw.org>
2017-03-09 15:50:27 +0100

    Add replacement for strndup on Solaris as it is no available

--- xmalloc.c
+++ xmalloc.c
@@ -23,6 +23,27 @@
 
 #include "tmux.h"
 
+#if defined(__sun)
+/* Written by Kaveh R. Ghazi <ghazi@caip.rutgers.edu> */
+char *
+strndup (const char *s, size_t n)
+{
+  char *result;
+  size_t len = strlen (s);
+
+  if (n < len)
+    len = n;
+
+  result = (char *) malloc (len + 1);
+  if (!result)
+    return 0;
+
+  memcpy (result, s, len);
+  result[len] = '\0';
+  return(result);
+}
+#endif
+
 void *
 xmalloc(size_t size)
 {
