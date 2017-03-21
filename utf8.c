c2b53598ad0130398e923135d5c80e0884356feb
Nicholas Marriott <nicholas.marriott@gmail.com>
2017-03-21 14:48:44 +0000

    Bad merge.

--- utf8.c
+++ utf8.c
@@ -115,7 +115,7 @@ utf8_width(wchar_t wc)
 	width = wcwidth(wc);
 #endif
 	if (width < 0 || width > 0xff) {
-		log_debug("Unicode %04x, wcwidth() %d", wc, width);
+		log_debug("Unicode %04lx, wcwidth() %d", (long)wc, width);
 
 #ifndef __OpenBSD__
 		/*
