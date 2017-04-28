bcd6b416749cbac712c29ee07049e98c5930c800
nicm <nicm>
2017-04-28 19:12:15 +0000

    Remove a reference from the right window when removing from a winlink's list.

--- window.c
+++ window.c
@@ -186,7 +186,7 @@ winlink_set_window(struct winlink *wl, struct window *w)
 {
 	if (wl->window != NULL) {
 		TAILQ_REMOVE(&wl->window->winlinks, wl, wentry);
-		window_remove_ref(w);
+		window_remove_ref(wl->window);
 	}
 	TAILQ_INSERT_TAIL(&w->winlinks, wl, wentry);
 	wl->window = w;
