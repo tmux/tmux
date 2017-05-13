7a4c66b7f5710e92f4af2bdfdb3b88bd64306320
nicm <nicm>
2017-05-13 07:41:59 +0000

    Scroll the right number of lines off the region when clearing.

--- tty.c
+++ tty.c
@@ -838,7 +838,7 @@ tty_clear_area(struct tty *tty, const struct window_pane *wp, u_int py,
 		    tty_term_has(tty->term, TTYC_INDN)) {
 			tty_region(tty, py, py + ny - 1);
 			tty_margin_off(tty);
-			tty_putcode1(tty, TTYC_INDN, ny);
+			tty_putcode1(tty, TTYC_INDN, ny - 1);
 			return;
 		}
 
@@ -853,7 +853,7 @@ tty_clear_area(struct tty *tty, const struct window_pane *wp, u_int py,
 		    tty_term_has(tty->term, TTYC_INDN)) {
 			tty_region(tty, py, py + ny - 1);
 			tty_margin(tty, px, px + nx - 1);
-			tty_putcode1(tty, TTYC_INDN, ny);
+			tty_putcode1(tty, TTYC_INDN, ny - 1);
 			return;
 		}
 	}
