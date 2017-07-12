fba6140a4a8258beb50114583fef2f5d62252157
nicm <nicm>
2017-07-12 12:35:31 +0000

    Do not need to set up USR2 twice.

--- proc.c
+++ proc.c
@@ -223,7 +223,6 @@ proc_set_signals(struct tmuxproc *tp, void (*signalcb)(int))
 
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGPIPE, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 	sigaction(SIGTSTP, &sa, NULL);
 
 	signal_set(&tp->ev_sighup, SIGHUP, proc_signal_cb, tp);
@@ -254,7 +253,6 @@ proc_clear_signals(struct tmuxproc *tp)
 
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGPIPE, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 	sigaction(SIGTSTP, &sa, NULL);
 
 	event_del(&tp->ev_sighup);
