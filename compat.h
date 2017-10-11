a3967de9a58f1773b8ca01a1f98fcafb86b73ac1
Nicholas Marriott <nicholas.marriott@gmail.com>
2017-10-11 08:03:31 +0100

    Include headers if found regardless of forkpty.

--- compat.h
+++ compat.h
@@ -101,16 +101,16 @@ void	warnx(const char *, ...);
 #include <paths.h>
 #endif
 
-#ifdef HAVE_FORKPTY
 #ifdef HAVE_LIBUTIL_H
 #include <libutil.h>
 #endif
+
 #ifdef HAVE_PTY_H
 #include <pty.h>
 #endif
+
 #ifdef HAVE_UTIL_H
 #include <util.h>
-#endif
 #endif
 
 #ifdef HAVE_VIS
