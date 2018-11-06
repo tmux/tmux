f9881b3b5de0c5ea8ee70dc95f5a5dd095e37bea
nicm <nicm>
2018-11-06 15:13:13 +0000

    Correctly unzoom and redraw panes in switch-client.

--- cmd-switch-client.c
+++ cmd-switch-client.c
@@ -113,8 +113,11 @@ cmd_switch_client_exec(struct cmd *self, struct cmdq_item *item)
 		if (item->client == NULL)
 			return (CMD_RETURN_NORMAL);
 		if (wl != NULL) {
-			if (wp != NULL)
+			server_unzoom_window(wl->window);
+			if (wp != NULL) {
+				window_redraw_active_switch(wp->window, wp);
 				window_set_active_pane(wp->window, wp);
+			}
 			session_set_current(s, wl);
 			cmd_find_from_session(&item->shared->current, s, 0);
 		}
