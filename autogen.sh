dd271bc6cdc545f308c0c31f22efeda5253e0d6a
andy5995 <andy400-dev@yahoo.com>
2018-12-01 23:18:07 -0600

    add -i flag to autoreconf

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,4 @@ die()
 }
 
 mkdir -p etc
-aclocal || die "aclocal failed"
-automake --add-missing --force-missing --copy --foreign || die "automake failed"
-autoreconf || die "autoreconf failed"
+autoreconf -i || die "autoreconf failed"
