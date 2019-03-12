b2bc34af12e6871faae82d1c46005e278d9cfe1d
nicm <nicm>
2019-03-12 23:21:45 +0000

    Set a flag on cells are genuinely empty (cleared and never written to) and use tty_clear_line (which will choose the best escape sequence) to clear any batches of cells with that flag when redrawing a line from the stored screen.

--- tmux.h
+++ tmux.h
@@ -579,6 +579,7 @@ enum utf8_state {
 #define GRID_FLAG_EXTENDED 0x8
 #define GRID_FLAG_SELECTED 0x10
 #define GRID_FLAG_NOPALETTE 0x20
+#define GRID_FLAG_CLEARED 0x40
 
 /* Grid line flags. */
 #define GRID_LINE_WRAPPED 0x1
