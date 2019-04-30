750d5830c2344d28bd6824681dfe5e3222933ef4
nicm <nicm>
2019-04-30 06:21:30 +0000

    Don't redraw control clients, from George Nachman.

--- cmd-select-pane.c
+++ cmd-select-pane.c
@@ -66,7 +66,7 @@ cmd_select_pane_redraw(struct window *w)
 	 */
 
 	TAILQ_FOREACH(c, &clients, entry) {
-		if (c->session == NULL)
+		if (c->session == NULL || (c->flags & CLIENT_CONTROL))
 			continue;
 		if (c->session->curw->window == w && tty_window_bigger(&c->tty))
 			server_redraw_client(c);
