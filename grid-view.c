b89f2f28bb5f21bfe836870d907ec6992acb5f28
nicm <nicm>
2019-07-16 10:30:56 +0000

    Fix grid clear code to correctly clear with the default background colour rather than ending up with the used count higher than the total size, GitHub issue 1829.

--- grid-view.c
+++ grid-view.c
@@ -214,7 +214,6 @@ grid_view_delete_cells(struct grid *gd, u_int px, u_int py, u_int nx, u_int bg)
 	sx = grid_view_x(gd, gd->sx);
 
 	grid_move_cells(gd, px, px + nx, py, sx - px - nx, bg);
-	grid_clear(gd, sx - nx, py, px + nx - (sx - nx), 1, bg);
 }
 
 /* Convert cells into a string. */
