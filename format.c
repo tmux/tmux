48c684cbc2b08d55c9790f05ac340e1a38e8bec4
nicm <nicm>
2019-09-24 14:50:08 +0000

    Mouse formats don't work in copy mode so don't try to use them.

--- format.c
+++ format.c
@@ -722,6 +722,8 @@ format_cb_mouse_word(struct format_tree *ft, struct format_entry *fe)
 	wp = cmd_mouse_pane(&ft->m, NULL, NULL);
 	if (wp == NULL)
 		return;
+	if (!TAILQ_EMPTY (&wp->modes))
+		return;
 	if (cmd_mouse_at(wp, &ft->m, &x, &y, 0) != 0)
 		return;
 	gd = wp->base.grid;
@@ -797,6 +799,8 @@ format_cb_mouse_line(struct format_tree *ft, struct format_entry *fe)
 		return;
 	wp = cmd_mouse_pane(&ft->m, NULL, NULL);
 	if (wp == NULL)
+		return;
+	if (!TAILQ_EMPTY (&wp->modes))
 		return;
 	if (cmd_mouse_at(wp, &ft->m, &x, &y, 0) != 0)
 		return;
