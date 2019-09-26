e4eb0a17556052582c265959847f8ee7eb842ade
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-09-25 20:50:33 -0700

    When a blank title is set re-enable automatic-rename

--- input.c
+++ input.c
@@ -2286,6 +2286,11 @@ input_exit_rename(struct input_ctx *ictx)
 
 	if (!utf8_isvalid(ictx->input_buf))
 		return;
+
+	if (ictx->input_len == 0) {
+		options_set_number(ictx->wp->window->options, "automatic-rename", 1);
+		return;
+	}
 	window_set_name(ictx->wp->window, ictx->input_buf);
 	options_set_number(ictx->wp->window->options, "automatic-rename", 0);
 	server_status_window(ictx->wp->window);
