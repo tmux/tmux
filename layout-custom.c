tmux/tmux

commit bbe8ebf9c26e45fd8c402627b84b3646db445d45

Some old tmux versions can sometimes generate layout strings which have the incorrect size for the top cell. Previously tmux didn't care but now that panes can be partly hidden, the size matters and is checked more strictly. So add some code to fix up the most common problem and a sanity check to reject layouts with any other size problems. Reported by Aleksandrs Ledovskis in GitHub issue 1930.

layout-custom.c
@@ -116,13 +116,49 @@ layout_append(struct layout_cell *lc, char *buf, size_t len)
 	return (0);
 }
 
+/* Check layout sizes fit. */
+static int
+layout_check(struct layout_cell *lc)
+{
+	struct layout_cell	*lcchild;
+	u_int			 n = 0;
+
+	switch (lc->type) {
+	case LAYOUT_WINDOWPANE:
+		break;
+	case LAYOUT_LEFTRIGHT:
+		TAILQ_FOREACH(lcchild, &lc->cells, entry) {
+			if (lcchild->sy != lc->sy)
+				return (0);
+			if (!layout_check(lcchild))
+				return (0);
+			n += lcchild->sx + 1;
+		}
+		if (n - 1 != lc->sx)
+			return (0);
+		break;
+	case LAYOUT_TOPBOTTOM:
+		TAILQ_FOREACH(lcchild, &lc->cells, entry) {
+			if (lcchild->sx != lc->sx)
+				return (0);
+			if (!layout_check(lcchild))
+				return (0);
+			n += lcchild->sy + 1;
+		}
+		if (n - 1 != lc->sy)
+			return (0);
+		break;
+	}
+	return (1);
+}
+
 /* Parse a layout string and arrange window as layout. */
 int
 layout_parse(struct window *w, const char *layout)
 {
 	struct layout_cell	*lc, *lcchild;
 	struct window_pane	*wp;
-	u_int			 npanes, ncells;
+	u_int			 npanes, ncells, sx = 0, sy = 0;
 	u_short			 csum;
 
 	/* Check validity. */
@@ -152,6 +188,37 @@ layout_parse(struct window *w, const char *layout)
 		lcchild = layout_find_bottomright(lc);
 		layout_destroy_cell(w, lcchild, &lc);
 	}
+
+	/*
+	 * It appears older versions of tmux were able to generate layouts with
+	 * an incorrect top cell size - if it is larger than the top child then
+	 * correct that (if this is still wrong the check code will catch it).
+	 */
+	switch (lc->type) {
+	case LAYOUT_WINDOWPANE:
+		break;
+	case LAYOUT_LEFTRIGHT:
+		TAILQ_FOREACH(lcchild, &lc->cells, entry) {
+			sy = lcchild->sy + 1;
+			sx += lcchild->sx + 1;
+		}
+		break;
+	case LAYOUT_TOPBOTTOM:
+		TAILQ_FOREACH(lcchild, &lc->cells, entry) {
+			sx = lcchild->sx + 1;
+			sy += lcchild->sy + 1;
+		}
+		break;
+	}
+	if (lc->sx != sx || lc->sy != sy) {
+		log_debug("fix layout %u,%u to %u,%u", lc->sx, lc->sy, sx,sy);
+		layout_print_cell(lc, __func__, 0);
+		lc->sx = sx - 1; lc->sy = sy - 1;
+	}
+
+	/* Check the new layout. */
+	if (!layout_check(lc))
+		return (-1);
 
 	/* Resize to the layout size. */
 	window_resize(w, lc->sx, lc->sy);

cmd-list-keys.c
@@ -125,7 +125,7 @@ cmd_list_keys_exec(struct cmd *self, struct cmdq_item *item)
 
 			cp = utf8_padcstr(table->name, tablewidth);
 			cplen = strlen(cp) + 1;
-			while (tmpused + cplen + 1>= tmpsize) {
+			while (tmpused + cplen + 1 >= tmpsize) {
 				tmpsize *= 2;
 				tmp = xrealloc(tmp, tmpsize);
 			}

commit de40e84c21deb01a2d305852d4d9023207db68e4

Fix select-layout in case the passed layout for more panes then window have
If you try to apply layout for two panes [1] for the window only with
one pane, tmux will SIGSEGV [2].
The reason is that lc->type will be LAYOUT_WINDOWPANE in this case and
it will try to adjust layout->sx to sx(==0)-1, and later it will fail in
screen_reset_tabs()

  [1]: "fdcb,273x73,0,0{175x73,0,0,221,97x73,176,0,225}"
  [2]:
       0  0x00005555555a2f8c in screen_reset_tabs (s=<optimized out>) at screen.c:134
       1  screen_reset_tabs (s=0x555555676870) at screen.c:125
       2  0x00005555555a3325 in screen_resize (s=s@entry=0x555555676870, sx=sx@entry=4294967295, sy=sy@entry=4294967295, reflow=1) at screen.c:209
       3  0x00005555555c7229 in window_pane_resize (wp=wp@entry=0x5555556766b0, sx=4294967295, sy=4294967295) at window.c:951
       4  0x000055555559395f in layout_fix_panes (w=w@entry=0x5555556763c0) at layout.c:308
       5  0x0000555555592175 in layout_parse (w=w@entry=0x5555556763c0, layout=<optimized out>, layout@entry=0x5555556292b0 "fdcb,273x73,0,0{175x73,0,0,221,97x73,176,0,225}") at layout-custom.c:236
       6  0x0000555555577c38 in cmd_select_layout_exec (self=<optimized out>, item=0x5555556c6d90) at cmd-select-layout.c:126
       7  0x00005555555756bc in cmdq_fire_command (item=0x5555556c6d90) at cmd-queue.c:303
       8  cmdq_next (c=c@entry=0x555555661630) at cmd-queue.c:438
       9  0x00005555555a89a6 in server_loop () at server.c:232
       10 0x000055555559cc62 in proc_loop (tp=0x5555556119f0, loopcb=loopcb@entry=0x5555555a8960 <server_loop>) at proc.c:201
       11 0x00005555555a906a in server_start (client=0x555555610f10, base=base@entry=0x555555610660, lockfd=lockfd@entry=5, lockfile=0x555555610a80 "\240\356eUUU") at server.c:213
       12 0x0000555555568cff in client_connect (start_server=1, path=0x55555560a040 "/tmp/tmux-1000/le", base=0x555555610660) at client.c:158
       13 client_main (base=0x555555610660, argc=6, argv=0x7fffffffd240, flags=<optimized out>) at client.c:266
       14 0x00005555555654c6 in main (argc=6, argv=<optimized out>) at tmux.c:375

Fixes: bbe8ebf9c26e45fd8c402627b84b3646db445d45

layout-custom.c
@@ -210,7 +210,7 @@ layout_parse(struct window *w, const char *layout)
 		}
 		break;
 	}
-	if (lc->sx != sx || lc->sy != sy) {
+	if (lc->type != LAYOUT_WINDOWPANE && (lc->sx != sx || lc->sy != sy)) {
 		log_debug("fix layout %u,%u to %u,%u", lc->sx, lc->sy, sx,sy);
 		layout_print_cell(lc, __func__, 0);
 		lc->sx = sx - 1; lc->sy = sy - 1;