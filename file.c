268f2b047a6e9b932a0ff51aa526e1df61d54165
nicm <nicm>
2019-12-12 11:51:32 +0000

    Do not check if client is dead if it is NULL.

--- file.c
+++ file.c
@@ -88,7 +88,7 @@ file_fire_done_cb(__unused int fd, __unused short events, void *arg)
 	struct client_file	*cf = arg;
 	struct client		*c = cf->c;
 
-	if (cf->cb != NULL && (~c->flags & CLIENT_DEAD))
+	if (cf->cb != NULL && (c == NULL || (~c->flags & CLIENT_DEAD)))
 		cf->cb(c, cf->path, cf->error, 1, cf->buffer, cf->data);
 	file_free(cf);
 }
