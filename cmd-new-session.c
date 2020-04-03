832b8a8cf51ff0ea5137ae38210e067ffae169d8
nicm <nicm>
2020-04-03 13:54:31 +0000

    Use new window and new pane as well for -P to new-session or new-window.

--- cmd-new-session.c
+++ cmd-new-session.c
@@ -334,7 +334,7 @@ cmd_new_session_exec(struct cmd *self, struct cmdq_item *item)
 	if (args_has(args, 'P')) {
 		if ((template = args_get(args, 'F')) == NULL)
 			template = NEW_SESSION_TEMPLATE;
-		cp = format_single(item, template, c, s, NULL, NULL);
+		cp = format_single(item, template, c, s, s->curw, NULL);
 		cmdq_print(item, "%s", cp);
 		free(cp);
 	}
