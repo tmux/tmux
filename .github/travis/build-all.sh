b203f7f19f972457fac840d95a662eb0e924f0c3
Nicholas Marriott <nicholas.marriott@gmail.com>
2020-05-03 12:03:50 +0100

    Better ncurses URL and some other fixes.

--- .github/travis/build-all.sh
+++ .github/travis/build-all.sh
@@ -4,31 +4,32 @@ BUILD=$PWD/build
 
 LIBEVENT=https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stab\
 le.tar.gz
-NCURSES=ftp://ftp.invisible-island.net/ncurses/ncurses-6.2.tar.gz
+NCURSES=https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz
 
-wget -q $LIBEVENT || exit 1
+wget -4q $LIBEVENT || exit 1
 tar -zxf libevent-*.tar.gz || exit 1
 (cd libevent-*/ &&
-         ./configure --prefix=$BUILD \
-                     --enable-shared \
-                     --disable-libevent-regress \
-                     --disable-samples &&
-         make && make install) || exit 1
+	 ./configure --prefix=$BUILD \
+		     --enable-shared \
+		     --disable-libevent-regress \
+		     --disable-samples &&
+	 make && make install) || exit 1
 
-wget -q $NCURSES || exit 1
+wget -4q $NCURSES || exit 1
 tar -zxf ncurses-*.tar.gz || exit 1
 (cd ncurses-*/ &&
-        CPPFLAGS=-P ./configure --prefix=$BUILD \
-                     --with-shared \
-                     --without-ada \
-                     --without-cxx \
-                     --without-manpages \
-                     --without-progs \
-                     --without-tests \
-                     --without-tack \
-                     --enable-pc-files \
-                     --with-pkg-config-libdir=$BUILD/lib/pkgconfig &&
-         make && make install) || exit 1
+	CPPFLAGS=-P ./configure --prefix=$BUILD \
+				--with-shared \
+				--with-termlib \
+				--without-ada \
+				--without-cxx \
+				--without-manpages \
+				--without-progs \
+				--without-tests \
+				--without-tack \
+				--enable-pc-files \
+				--with-pkg-config-libdir=$BUILD/lib/pkgconfig &&
+	 make && make install) || exit 1
 
 sh autogen.sh || exit 1
 PKG_CONFIG_PATH=$BUILD/lib/pkgconfig ./configure --prefix=$BUILD "$@"
