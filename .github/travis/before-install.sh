97d490204b9dd2f3bc3b5d54a1321b084f2021d6
Nicholas Marriott <nicholas.marriott@gmail.com>
2020-05-03 11:47:28 +0100

    Add build of everything.

--- .github/travis/before-install.sh
+++ .github/travis/before-install.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	sudo apt-get update -qq
 	sudo apt-get -y install bison \
-			        autotools-dev \
+				autotools-dev \
 				libncurses5-dev \
 				libevent-dev \
 				pkg-config \
--- .github/travis/build.sh
+++ .github/travis/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 sh autogen.sh || exit 1
-if [ "$BUILD" = "static" ]; then
-	./configure --enable-static || exit 1
-else
-	./configure || exit 1
-fi
-exec make
+case "$BUILD" in
+	static)
+		./configure --enable-static || exit 1
+		exec make
+		;;
+	all)
+		sh $(dirname $0)/build-all.sh
+		exec make
+		;;
+	*)
+		./configure || exit 1
+		exec make
+		;;
+esac
