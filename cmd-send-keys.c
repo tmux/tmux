2cf967ee6786776675634459eefdff3dd7d7b778
Nicholas Marriott <nicholas.marriott@gmail.com>
2020-05-15 11:20:12 +0100

    Always set xterm flag.

--- cmd-send-keys.c
+++ cmd-send-keys.c
@@ -71,9 +71,7 @@ cmd_send_keys_inject_key(struct cmdq_item *item, struct cmdq_item *after,
 
 	wme = TAILQ_FIRST(&wp->modes);
 	if (wme == NULL || wme->mode->key_table == NULL) {
-		if (options_get_number(wp->window->options, "xterm-keys"))
-			key |= KEYC_XTERM;
-		if (window_pane_key(wp, tc, s, wl, key, NULL) != 0)
+		if (window_pane_key(wp, tc, s, wl, key|KEYC_XTERM, NULL) != 0)
 			return (NULL);
 		return (item);
 	}
