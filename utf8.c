bbfb44e9b2e5b2e6560abb0892bb9d5f68e42d0f
nicm <nicm>
2020-05-25 15:02:25 +0000

    Make some data types consistent.

--- utf8.c
+++ utf8.c
@@ -55,7 +55,7 @@ static u_int utf8_big_list_size;
 static u_int utf8_big_list_used;
 
 union utf8_big_map {
-	uint32_t	value;
+	u_int	value;
 	struct {
 		u_char	flags;
 #define UTF8_BIG_SIZE 0x1f
@@ -125,7 +125,7 @@ utf8_put_big_item(const char *data, size_t size, u_int *index)
 }
 
 /* Get UTF-8 as index into buffer. */
-uint32_t
+u_int
 utf8_map_big(const struct utf8_data *ud)
 {
 	union utf8_big_map	 m = { .value = 0 };
@@ -165,7 +165,7 @@ fail:
 
 /* Get UTF-8 from index into buffer. */
 void
-utf8_get_big(uint32_t v, struct utf8_data *ud)
+utf8_get_big(u_int v, struct utf8_data *ud)
 {
 	union utf8_big_map	 m = { .value = v };
 	struct utf8_big_item	*bi;
@@ -183,7 +183,7 @@ utf8_get_big(uint32_t v, struct utf8_data *ud)
 		return;
 	}
 
-	o = ((uint32_t)m.data[2] << 16)|((uint32_t)m.data[1] << 8)|m.data[0];
+	o = ((u_int)m.data[2] << 16)|((u_int)m.data[1] << 8)|m.data[0];
 	if (o >= utf8_big_list_used)
 		memset(ud->data, ' ', ud->size);
 	else {
@@ -193,7 +193,7 @@ utf8_get_big(uint32_t v, struct utf8_data *ud)
 }
 
 /* Get big value for UTF-8 single character. */
-uint32_t
+u_int
 utf8_set_big(char c, u_int width)
 {
 	union utf8_big_map	m = { .flags = 1, .data[0] = c };
