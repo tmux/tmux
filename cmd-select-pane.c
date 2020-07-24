d329b035cee47d968a8c93b5cbd8fde879ce6f0d
nicm <nicm>
2020-07-24 07:05:37 +0000

    Add a hook when the pane title changed.

--- cmd-select-pane.c
+++ cmd-select-pane.c
@@ -198,6 +198,7 @@ cmd_select_pane_exec(struct cmd *self, struct cmdq_item *item)
 	if (args_has(args, 'T')) {
 		title = format_single_from_target(item, args_get(args, 'T'));
 		if (screen_set_title(&wp->base, title)) {
+			notify_pane("pane-title-changed", wp);
 			server_redraw_window_borders(wp->window);
 			server_status_window(wp->window);
 		}
