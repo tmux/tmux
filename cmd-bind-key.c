1fed7e84a3d65c8fbfbb321b84236ccab7265d46
nicm <nicm>
2020-09-08 10:19:19 +0000

    Allow -N without a command to change or add a note to an existing key.

--- cmd-bind-key.c
+++ cmd-bind-key.c
@@ -33,9 +33,9 @@ const struct cmd_entry cmd_bind_key_entry = {
 	.name = "bind-key",
 	.alias = "bind",
 
-	.args = { "nrN:T:", 2, -1 },
+	.args = { "nrN:T:", 1, -1 },
 	.usage = "[-nr] [-T key-table] [-N note] key "
-	         "command [arguments]",
+	         "[command [arguments]]",
 
 	.flags = CMD_AFTERHOOK,
 	.exec = cmd_bind_key_exec
@@ -46,7 +46,7 @@ cmd_bind_key_exec(struct cmd *self, struct cmdq_item *item)
 {
 	struct args		 *args = cmd_get_args(self);
 	key_code		  key;
-	const char		 *tablename, *note;
+	const char		 *tablename, *note = args_get(args, 'N');
 	struct cmd_parse_result	 *pr;
 	char			**argv = args->argv;
 	int			  argc = args->argc, repeat;
@@ -65,22 +65,24 @@ cmd_bind_key_exec(struct cmd *self, struct cmdq_item *item)
 		tablename = "prefix";
 	repeat = args_has(args, 'r');
 
-	if (argc == 2)
-		pr = cmd_parse_from_string(argv[1], NULL);
-	else
-		pr = cmd_parse_from_arguments(argc - 1, argv + 1, NULL);
-	switch (pr->status) {
-	case CMD_PARSE_EMPTY:
-		cmdq_error(item, "empty command");
-		return (CMD_RETURN_ERROR);
-	case CMD_PARSE_ERROR:
-		cmdq_error(item, "%s", pr->error);
-		free(pr->error);
-		return (CMD_RETURN_ERROR);
-	case CMD_PARSE_SUCCESS:
-		break;
-	}
-	note = args_get(args, 'N');
-	key_bindings_add(tablename, key, note, repeat, pr->cmdlist);
+	if (argc != 1) {
+		if (argc == 2)
+			pr = cmd_parse_from_string(argv[1], NULL);
+		else
+			pr = cmd_parse_from_arguments(argc - 1, argv + 1, NULL);
+		switch (pr->status) {
+		case CMD_PARSE_EMPTY:
+			cmdq_error(item, "empty command");
+			return (CMD_RETURN_ERROR);
+		case CMD_PARSE_ERROR:
+			cmdq_error(item, "%s", pr->error);
+			free(pr->error);
+			return (CMD_RETURN_ERROR);
+		case CMD_PARSE_SUCCESS:
+			break;
+		}
+		key_bindings_add(tablename, key, note, repeat, pr->cmdlist);
+	} else
+		key_bindings_add(tablename, key, note, repeat, NULL);
 	return (CMD_RETURN_NORMAL);
 }
