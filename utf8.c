0ecba2336db4e09b0a93195cf5d5a48178c07ccb
Mike <15933146+example@users.noreply.github.com>
2020-09-14 16:29:12 +0200

    fix two warnings on unused variable index

--- utf8.c
+++ utf8.c
@@ -100,9 +100,8 @@ utf8_put_item(const char *data, size_t size, u_int *index)
 
 	ui = utf8_item_by_data(data, size);
 	if (ui != NULL) {
-		log_debug("%s: found %.*s = %u", __func__, (int)size, data,
-		    *index);
 		*index = ui->index;
+		log_debug("%s: found %.*s = %u", __func__, (int)size, data, *index);
 		return (0);
 	}
 
@@ -117,8 +116,8 @@ utf8_put_item(const char *data, size_t size, u_int *index)
 	ui->size = size;
 	RB_INSERT(utf8_data_tree, &utf8_data_tree, ui);
 
-	log_debug("%s: added %.*s = %u", __func__, (int)size, data, *index);
 	*index = ui->index;
+	log_debug("%s: added %.*s = %u", __func__, (int)size, data, *index);
 	return (0);
 }
 
