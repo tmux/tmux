0dcf93aa8f592750d6a50003759a61a2f1ceb68a
Mike <15933146+example@users.noreply.github.com>
2020-09-16 11:50:55 +0200

    warning on llx fixed: age is uint64_t printf with lx

--- control.c
+++ control.c
@@ -687,7 +687,7 @@ control_write_pending(struct client *c, struct control_pane *cp, size_t limit)
 			age = t - cb->t;
 		else
 			age = 0;
-		log_debug("%s: %s: output block %zu (age %llu) for %%%u "
+		log_debug("%s: %s: output block %zu (age %lu) for %%%u "
 		    "(used %zu/%zu)", __func__, c->name, cb->size, age,
 		    cp->pane, used, limit);
 
