bc4681c83d612a3d9609dd609e7d89b035b25dd5
nicm <nicm>
2021-04-13 05:25:05 +0000

    Move mode set/reset after sync so cursor doesn't flicker, from Avi Halachmi.

--- screen-redraw.c
+++ screen-redraw.c
@@ -604,8 +604,8 @@ screen_redraw_screen(struct client *c)
 		return;
 
 	screen_redraw_set_context(c, &ctx);
-	tty_update_mode(&c->tty, c->tty.mode, NULL);
 	tty_sync_start(&c->tty);
+	tty_update_mode(&c->tty, c->tty.mode, NULL);
 
 	if (flags & (CLIENT_REDRAWWINDOW|CLIENT_REDRAWBORDERS)) {
 		log_debug("%s: redrawing borders", c->name);
@@ -640,8 +640,8 @@ screen_redraw_pane(struct client *c, struct window_pane *wp)
 		return;
 
 	screen_redraw_set_context(c, &ctx);
-	tty_update_mode(&c->tty, c->tty.mode, NULL);
 	tty_sync_start(&c->tty);
+	tty_update_mode(&c->tty, c->tty.mode, NULL);
 
 	screen_redraw_draw_pane(&ctx, wp);
 
