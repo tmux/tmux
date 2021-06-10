9f38a8807c6c6b3b7596f06c2bb662242337ac48
nicm <nicm>
2021-06-10 07:24:45 +0000

    Include current client in size calculation for new sessions, GitHub issue 2662.

--- cmd-new-session.c
+++ cmd-new-session.c
@@ -269,6 +269,7 @@ cmd_new_session_exec(struct cmd *self, struct cmdq_item *item)
 	memset(&sc, 0, sizeof sc);
 	sc.item = item;
 	sc.s = s;
+	sc.tc = c;
 
 	sc.name = args_get(args, 'n');
 	sc.argc = args->argc;
