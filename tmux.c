tmux/tmux

commit f32fd2df69d4fec0476507e600a0363b35059f2b

Improve error message if creating socket parent directory fails, from Thomas Adam for GitHub issue 1215.

tmux.c
@@ -47,7 +47,7 @@ int		 ptm_fd = -1;
 const char	*shell_command;
 
 static __dead void	 usage(void);
-static char		*make_label(const char *);
+static char		*make_label(const char *, char **);
 
 static const char	*getshell(void);
 static int		 checkshell(const char *);
@@ -109,12 +109,13 @@ areshell(const char *shell)
 }
 
 static char *
-make_label(const char *label)
+make_label(const char *label, char **cause)
 {
 	char		*base, resolved[PATH_MAX], *path, *s;
 	struct stat	 sb;
 	uid_t		 uid;
-	int		 saved_errno;
+
+	*cause = NULL;
 
 	if (label == NULL)
 		label = "default";
@@ -124,11 +125,16 @@ make_label(const char *label)
 		xasprintf(&base, "%s/tmux-%ld", s, (long)uid);
 	else
 		xasprintf(&base, "%s/tmux-%ld", _PATH_TMP, (long)uid);
-
-	if (mkdir(base, S_IRWXU) != 0 && errno != EEXIST)
+	if (realpath(base, resolved) == NULL &&
+	    strlcpy(resolved, base, sizeof resolved) >= sizeof resolved) {
+		errno = ERANGE;
+		free(base);
 		goto fail;
+	}
 
-	if (lstat(base, &sb) != 0)
+	if (mkdir(resolved, S_IRWXU) != 0 && errno != EEXIST)
+		goto fail;
+	if (lstat(resolved, &sb) != 0)
 		goto fail;
 	if (!S_ISDIR(sb.st_mode)) {
 		errno = ENOTDIR;
@@ -138,18 +144,11 @@ make_label(const char *label)
 		errno = EACCES;
 		goto fail;
 	}
-
-	if (realpath(base, resolved) == NULL)
-		strlcpy(resolved, base, sizeof resolved);
 	xasprintf(&path, "%s/%s", resolved, label);
-
-	free(base);
 	return (path);
 
 fail:
-	saved_errno = errno;
-	free(base);
-	errno = saved_errno;
+	xasprintf(cause, "error creating %s (%s)", resolved, strerror(errno));
 	return (NULL);
 }
 
@@ -191,7 +190,7 @@ find_home(void)
 int
 main(int argc, char **argv)
 {
-	char					*path, *label, **var;
+	char					*path, *label, *cause, **var;
 	char					 tmp[PATH_MAX];
 	const char				*s, *shell, *cwd;
 	int					 opt, flags, keys;
@@ -341,8 +340,11 @@ main(int argc, char **argv)
 			path[strcspn(path, ",")] = '\0';
 		}
 	}
-	if (path == NULL && (path = make_label(label)) == NULL) {
-		fprintf(stderr, "can't create socket: %s\n", strerror(errno));
+	if (path == NULL && (path = make_label(label, &cause)) == NULL) {
+		if (cause != NULL) {
+			fprintf(stderr, "%s\n", cause);
+			free(cause);
+		}
 		exit(1);
 	}
 	socket_path = path;

commit d0d12433dc5ee98b845055f96ce93b1482f6349f

Improve error message if socket dir has too liberal permissions
tmux expects that the directory containing the socket used to
communicate with the server process to be owned by the user and that
others have no permission to access it.

If this fails however, the emitted error message is misleading as the
directory exists just fine:

	uwe@taurus:~$ chmod o+x /tmp/tmux-1000
	uwe@taurus:~$ tmux a
	error creating /tmp/tmux-1000 (Permission denied)

Rework error handling in make_label() to emit an error message that
talks about unsafe permissions in this case instead.

Debian-Bug: https://bugs.debian.org/990215
Fixes: f32fd2df69d4 ("Improve error message if creating socket parent directory fails [...]")

tmux.c
@@ -207,16 +207,19 @@ make_label(const char *label, char **cause)
 	free(paths);
 
 	xasprintf(&base, "%s/tmux-%ld", path, (long)uid);
-	if (mkdir(base, S_IRWXU) != 0 && errno != EEXIST)
+	if ((mkdir(base, S_IRWXU) != 0 && errno != EEXIST) ||
+	    lstat(base, &sb) != 0) {
+		xasprintf(cause, "error creating %s (%s)", base, strerror(errno));
 		goto fail;
-	if (lstat(base, &sb) != 0)
-		goto fail;
+	}
 	if (!S_ISDIR(sb.st_mode)) {
 		errno = ENOTDIR;
+		xasprintf(cause, "%s is not a directory", base);
 		goto fail;
 	}
 	if (sb.st_uid != uid || (sb.st_mode & S_IRWXO) != 0) {
 		errno = EACCES;
+		xasprintf(cause, "unsafe permissions for %s", base);
 		goto fail;
 	}
 	xasprintf(&path, "%s/%s", base, label);
@@ -224,7 +227,6 @@ make_label(const char *label, char **cause)
 	return (path);
 
 fail:
-	xasprintf(cause, "error creating %s (%s)", base, strerror(errno));
 	free(base);
 	return (NULL);
 }