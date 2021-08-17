158f0e8c414e9525358189bd7135e12e6e6ada49
nicm <nicm>
2021-08-17 07:14:33 +0000

    Start sync before drawing popup.

--- popup.c
+++ popup.c
@@ -554,6 +554,7 @@ popup_job_update_cb(struct job *job)
 	if (size == 0)
 		return;
 
+	tty_sync_start(&c->tty);
 	if (pd->md != NULL) {
 		c->overlay_check = menu_check_cb;
 		c->overlay_data = pd->md;
