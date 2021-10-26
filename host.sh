4080fc603d3715ff78a15c423db2553687f526a8
Holland Schutte <schutth@wwu.edu>
2021-10-25 23:03:28 -0700

    since this is a testing script, we can afford to remove log files

--- host.sh
+++ host.sh
@@ -2,6 +2,7 @@
 export TMUX_SOCKET_FILE=/tmp/tmux.socket
 
 rm -f $TMUX_SOCKET_FILE
+rm -f tmux-*.log
 
 #gdb --args ./tmux -S $TMUX_SOCKET_FILE new-session -s sess
 ./tmux -S $TMUX_SOCKET_FILE new-session -s sess
