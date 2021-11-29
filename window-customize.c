add03dfb8dd4f2871b2cbc487ff5af95a63f48f4
nicm <nicm>
2021-11-29 11:01:51 +0000

    Fix user option lookup ordering.

--- window-customize.c
+++ window-customize.c
@@ -398,11 +398,11 @@ window_customize_build_options(struct window_customize_modedata *data,
 
 	for (i = 0; i < size; i++) {
 		if (oo2 != NULL)
-			o = options_get(oo0, list[i]);
+			o = options_get(oo2, list[i]);
 		if (o == NULL && oo1 != NULL)
 			o = options_get(oo1, list[i]);
 		if (o == NULL)
-			o = options_get(oo2, list[i]);
+			o = options_get(oo0, list[i]);
 		if (options_owner(o) == oo2)
 			scope = scope2;
 		else if (options_owner(o) == oo1)
