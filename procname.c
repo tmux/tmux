71c3234dc78f6f59a66757a6e272e77823a820b3
deraadt <deraadt>
2021-12-07 00:38:42 +0000

    Use PATH_MAX (the standard name) rather than MAXPATHLEN (from BSD sys/param.h)

--- procname.c
+++ procname.c
@@ -138,7 +138,7 @@ char *
 get_proc_cwd(int fd)
 {
         int             name[] = { CTL_KERN, KERN_PROC_CWD, 0 };
-        static char     path[MAXPATHLEN];
+        static char     path[PATH_MAX];
         size_t          pathlen = sizeof path;
 
         if ((name[2] = tcgetpgrp(fd)) == -1)
