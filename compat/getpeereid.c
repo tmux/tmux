6a9bb2a6224958474469422e3b9d667821d0df74
Nicholas Marriott <nicholas.marriott@gmail.com>
2022-06-02 21:44:28 +0100

    Remove extra definition of getpeereid. From Eric N Vander Weele in GitHub issue 3209.

--- compat/getpeereid.c
+++ compat/getpeereid.c
@@ -38,9 +38,6 @@ getpeereid(int s, uid_t *uid, gid_t *gid)
 	*gid = uc.gid;
 	return (0);
 #elif defined(HAVE_GETPEERUCRED)
-int
-getpeereid(int s, uid_t *uid, gid_t *gid)
-{
         ucred_t *ucred = NULL;
 
         if (getpeerucred(s, &ucred) == -1)
@@ -51,7 +48,6 @@ getpeereid(int s, uid_t *uid, gid_t *gid)
                 return (-1);
         ucred_free(ucred);
         return (0);
-}
 #else
 	return (getuid());
 #endif
