746ab37dc7f69f7089d64af17139624f7f74ff7e
Jeff <jeff999955@gmail.com>
2023-06-21 14:02:53 +0800

    Add osx build script

---
+++ osx_build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# git submodule update --init --recursive
+
+uid=$((id -u))
+if [ $uid != 0 ]; then
+    echo You will need elevated privilege to install the dependencies
+    sudo -s
+fi
+
+submodule_path="./modules"
+for f in `ls $submodule_path`; do
+    cmake $submodule_path/$f -B $submodule_path/build
+    sudo cmake --build $submodule_path/build --target install --parallel 4
+    rm -rf $submodule_path/build
+done
