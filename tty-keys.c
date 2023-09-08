c02bc4dbe9692fb8f698509a35c3abccf895f263
nicm <nicm>
2023-09-08 07:05:06 +0000

    On second thoughts, do check DA2 for DECFRA and DECSLRM since that will catch terminals that say they are VT520 even if we can't use DA1 (because of VTE).

--- tty-keys.c
+++ tty-keys.c
@@ -1399,8 +1399,17 @@ tty_keys_device_attributes2(struct tty *tty, const char *buf, size_t len,
 			break;
 	}
 
-	/* Add terminal features. */
+	/*
+	 * Add terminal features. We add DECSLRM and DECFRA for some
+	 * identification codes here, notably 64 will catch VT520, even though
+	 * we can't use level 5 from DA because of VTE.
+	 */
 	switch (p[0]) {
+	case 41: /* VT420 */
+	case 61: /* VT510 */
+	case 64: /* VT520 */
+		tty_add_features(features, "margins,rectfill", ",");
+		break;
 	case 'M': /* mintty */
 		tty_default_features(features, "mintty", 0);
 		break;
