9ba433e521434abdd82babec1041d64b5f5c94ef
Nicholas Marriott <nicholas.marriott@gmail.com>
2024-05-13 12:42:14 +0100

    Use printf not echo -e, from Joyce Lin.

--- regress/copy-mode-test-vi.sh
+++ regress/copy-mode-test-vi.sh
@@ -41,14 +41,14 @@ $TMUX send-keys -X begin-selection
 $TMUX send-keys -X next-word-end
 $TMUX send-keys -X next-word-end
 $TMUX send-keys -X copy-selection
-[ "$($TMUX show-buffer)" = "$(echo -e "words\n        Indented")" ] || exit 1
+[ "$($TMUX show-buffer)" = "$(printf "words\n        Indented")" ] || exit 1
 
 # Test that `next-word` wraps around un-indented line breaks.
 $TMUX send-keys -X next-word
 $TMUX send-keys -X begin-selection
 $TMUX send-keys -X next-word
 $TMUX send-keys -X copy-selection
-[ "$($TMUX show-buffer)" = "$(echo -e "line\nA")" ] || exit 1
+[ "$($TMUX show-buffer)" = "$(printf "line\nA")" ] || exit 1
 
 # Test that `next-word-end` does not treat periods as letters.
 $TMUX send-keys -X next-word
@@ -69,7 +69,7 @@ $TMUX send-keys -X previous-space
 $TMUX send-keys -X begin-selection
 $TMUX send-keys -X next-space
 $TMUX send-keys -X copy-selection
-[ "$($TMUX show-buffer)" = "$(echo -e "line...\n.")" ] || exit 1
+[ "$($TMUX show-buffer)" = "$(printf "line...\n.")" ] || exit 1
 
 # Test that `next-word` and `next-word-end` do not treat other symbols as letters.
 $TMUX send-keys -X begin-selection
@@ -85,7 +85,7 @@ $TMUX send-keys -X next-space
 $TMUX send-keys -X begin-selection
 $TMUX send-keys -X next-space
 $TMUX send-keys -X copy-selection
-[ "$($TMUX show-buffer)" = "$(echo -e "\$ym_bols[]{}\n ?")" ] || exit 1
+[ "$($TMUX show-buffer)" = "$(printf "\$ym_bols[]{}\n ?")" ] || exit 1
 
 # Test that `next-word-end` treats digits as letters
 $TMUX send-keys -X next-word-end
