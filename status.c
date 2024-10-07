31e1a4387151a50f325b3154f3aac21f73b01877
Alexander Arkhipov <aa@manpager.org>
2024-10-07 16:40:44 +0400

    remove cursorgc from prompt redrawing

--- status.c
+++ status.c
@@ -722,7 +722,7 @@ status_prompt_redraw(struct client *c)
 	struct screen		 old_screen;
 	u_int			 i, lines, offset, left, start, width;
 	u_int			 pcursor, pwidth, promptline;
-	struct grid_cell	 gc, cursorgc;
+	struct grid_cell	 gc;
 	struct format_tree	*ft;
 
 	if (c->tty.sx == 0 || c->tty.sy == 0)
@@ -744,8 +744,6 @@ status_prompt_redraw(struct client *c)
 	else
 		style_apply(&gc, s->options, "message-style", ft);
 	format_free(ft);
-
-	memcpy(&cursorgc, &gc, sizeof cursorgc);
 
 	start = format_width(c->prompt_string);
 	if (start > c->tty.sx)
@@ -791,16 +789,9 @@ status_prompt_redraw(struct client *c)
 		if (width > offset + pwidth)
 			break;
 
-		if (i != c->prompt_index) {
-			utf8_copy(&gc.data, &c->prompt_buffer[i]);
-			screen_write_cell(&ctx, &gc);
-		} else {
-			utf8_copy(&cursorgc.data, &c->prompt_buffer[i]);
-			screen_write_cell(&ctx, &cursorgc);
-		}
+		utf8_copy(&gc.data, &c->prompt_buffer[i]);
+		screen_write_cell(&ctx, &gc);
 	}
-	if (sl->active->cx < screen_size_x(sl->active) && c->prompt_index >= i)
-		screen_write_putc(&ctx, &cursorgc, ' ');
 
 finished:
 	screen_write_stop(&ctx);
