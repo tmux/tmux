040d21412394ffbe3d4513aa5d345b319b67cb15
Václav Kadlčík <vkadlcik@redhat.com>
2025-09-06 14:26:42 +0200

    Optionally limit number of columns in tiled layout
    Introduce a new window option: tiled-layout-max-columns. It configures
    the maximum number of columns in the tiled layout. The default value
    is 0 which means no limit and makes the change backward-compatible.

--- layout-set.c
+++ layout-set.c
@@ -587,7 +587,7 @@ layout_set_tiled(struct window *w)
 	struct window_pane	*wp;
 	struct layout_cell	*lc, *lcrow, *lcchild;
 	u_int			 n, width, height, used, sx, sy;
-	u_int			 i, j, columns, rows;
+	u_int			 i, j, columns, rows, max_columns;
 
 	layout_print_cell(w->layout_root, __func__, 1);
 
@@ -596,11 +596,14 @@ layout_set_tiled(struct window *w)
 	if (n <= 1)
 		return;
 
+	/* Get maximum columns from window option. */
+	max_columns = options_get_number(w->options, "tiled-layout-max-columns");
+
 	/* How many rows and columns are wanted? */
 	rows = columns = 1;
 	while (rows * columns < n) {
 		rows++;
-		if (rows * columns < n)
+		if (rows * columns < n && (max_columns == 0 || columns < max_columns))
 			columns++;
 	}
 
