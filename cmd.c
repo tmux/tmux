cc83ca66288b4b9763a4e63818461da4c4f42075
Michael Grant <mgrant@grant.org>
2025-10-26 14:02:57 +0000

    Add new newp command.

--- cmd.c
+++ cmd.c
@@ -109,7 +109,7 @@ extern const struct cmd_entry cmd_show_prompt_history_entry;
 extern const struct cmd_entry cmd_show_window_options_entry;
 extern const struct cmd_entry cmd_source_file_entry;
 extern const struct cmd_entry cmd_split_window_entry;
-extern const struct cmd_entry cmd_new_floating_window_entry;
+extern const struct cmd_entry cmd_new_pane_entry;
 extern const struct cmd_entry cmd_start_server_entry;
 extern const struct cmd_entry cmd_suspend_client_entry;
 extern const struct cmd_entry cmd_swap_pane_entry;
@@ -202,7 +202,7 @@ const struct cmd_entry *cmd_table[] = {
 	&cmd_show_window_options_entry,
 	&cmd_source_file_entry,
 	&cmd_split_window_entry,
-	&cmd_new_floating_window_entry,
+	&cmd_new_pane_entry,
 	&cmd_start_server_entry,
 	&cmd_suspend_client_entry,
 	&cmd_swap_pane_entry,
