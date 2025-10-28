aa420cd54a5813cefd4bda16c07a4ba379956034
nicm <nicm>
2025-10-28 07:32:26 +0000

    Tweak error messages so that file name isn't modified when we capitalize the first letter, from Pavel Roskin in GitHub issue 4666.

--- cmd-save-buffer.c
+++ cmd-save-buffer.c
@@ -66,7 +66,7 @@ cmd_save_buffer_done(__unused struct client *c, const char *path, int error,
 		return;
 
 	if (error != 0)
-		cmdq_error(item, "%s: %s", path, strerror(error));
+		cmdq_error(item, "%s: %s", strerror(error), path);
 	cmdq_continue(item);
 }
 
