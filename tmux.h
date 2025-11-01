d90b414223021378966f8b6988b725872e237793
nicm <nicm>
2025-11-01 16:44:24 +0000

    Handle regional indicators and emoji modifiers in a better way, GitHub issue 3998.

--- tmux.h
+++ tmux.h
@@ -3499,7 +3499,8 @@ int		 utf8_cstrhas(const char *, const struct utf8_data *);
 int		 utf8_has_zwj(const struct utf8_data *);
 int		 utf8_is_zwj(const struct utf8_data *);
 int		 utf8_is_vs(const struct utf8_data *);
-int		 utf8_is_modifier(const struct utf8_data *);
+int		 utf8_should_combine(const struct utf8_data *,
+		    const struct utf8_data *);
 enum hanguljamo_state hanguljamo_check_state(const struct utf8_data *,
 		    const struct utf8_data *);
 
