d2fa20a39049b99b9d4038164aecaffbc13b4611
tb <tb>
2026-03-04 07:19:32 +0000

    tmux: use VIS_NOSLASH to avoid annoying double escaping on paste
    from nicm

--- cmd-paste-buffer.c
+++ cmd-paste-buffer.c
@@ -50,7 +50,7 @@ cmd_paste_buffer_paste(struct window_pane *wp, const char *buf, size_t len)
 	char	*cp;
 	size_t	 n;
 
-	n = utf8_stravisx(&cp, buf, len, VIS_SAFE);
+	n = utf8_stravisx(&cp, buf, len, VIS_SAFE|VIS_NOSLASH);
 	bufferevent_write(wp->event, cp, n);
 	free(cp);
 }
