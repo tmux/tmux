d699fe5d46a755d12af8ce55d7d0a6e5e5be61ee
Patrik Sundberg <patrik.sundberg@gmail.com>
2026-03-13 13:50:47 +0000

    Update copyright to 2026 Patrik Sundberg in new kitty files

--- input-kitty.c
+++ input-kitty.c
@@ -1,7 +1,7 @@
 /* $OpenBSD$ */
 
 /*
- * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
+ * Copyright (c) 2026 Patrik Sundberg <patrik.sundberg@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
