297ddfe3f741a17a132375ffd1e3785d63d4d118
Dane Jensen <dhcjensen@gmail.com>
2026-04-05 14:15:52 -0700

    Adjusted behavior to use the already existing mechanisms of `automatic-rename`

--- server-client.c
+++ server-client.c
@@ -50,7 +50,6 @@ static key_code	server_client_check_mouse(struct client *, struct key_event *);
 static void	server_client_repeat_timer(int, short, void *);
 static void	server_client_click_timer(int, short, void *);
 static void	server_client_check_exit(struct client *);
-static void	server_client_check_fg_pids(struct client *);
 static void	server_client_check_redraw(struct client *);
 static void	server_client_check_modes(struct client *);
 static void	server_client_set_title(struct client *);
@@ -2763,6 +2762,7 @@ server_client_loop(void)
 	struct window_pane		*wp, *twp;
 	struct window_mode_entry	*wme;
 	u_int				 bit;
+	pid_t				 pid;
 
 	/* Check for window resize. This is done before redrawing. */
 	RB_FOREACH(w, windows, &windows)
@@ -2785,7 +2785,6 @@ server_client_loop(void)
 		server_client_check_exit(c);
 		if (c->session != NULL && c->session->curw != NULL) {
 			server_client_check_modes(c);
-			server_client_check_fg_pids(c);
 			server_client_check_redraw(c);
 			server_client_reset_state(c);
 		}
@@ -2800,6 +2799,10 @@ server_client_loop(void)
 			if (wp->fd != -1) {
 				server_client_check_pane_resize(wp);
 				server_client_check_pane_buffer(wp);
+			}
+			if ((pid = tcgetpgrp(wp->fd)) != wp->fg_pid) {
+				wp->fg_pid = pid;
+				wp->flags |= PANE_CHANGED;
 			}
 			/*
 			 * If PANE_REDRAW was set during buffer processing
@@ -3273,27 +3276,6 @@ server_client_check_modes(struct client *c)
 		wme = TAILQ_FIRST(&wp->modes);
 		if (wme != NULL && wme->mode->update != NULL)
 			wme->mode->update(wme);
-	}
-}
-
-/* Check for client foregroup pid changes in client's panes. Used for
- * triggering a status redraw.
- */
-static void
-server_client_check_fg_pids(struct client *c)
-{
-	struct session		*s = c->session;
-	struct winlink		*wl = s->curw;
-	struct window		*w = wl->window;
-	struct window_pane	*wp;
-	pid_t			 pid;
-
-	TAILQ_FOREACH(wp, &w->panes, entry) {
-		if ((pid = tcgetpgrp(wp->fd)) != wp->fg_pid) {
-			wp->fg_pid = pid;
-			c->flags |= CLIENT_REDRAWSTATUS;
-			return;
-		}
 	}
 }
 
