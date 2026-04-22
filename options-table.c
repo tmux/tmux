918b7dcc1f079b8ec919d963c97c54647f3e9239
leohenon <77656081+lhenon999@users.noreply.github.com>
2026-04-22 22:15:24 +0800

    Add copy mode line number options

--- options-table.c
+++ options-table.c
@@ -108,6 +108,9 @@ static const char *options_table_extended_keys_format_list[] = {
 static const char *options_table_allow_passthrough_list[] = {
 	"off", "on", "all", NULL
 };
+static const char *options_table_copy_mode_line_numbers_list[] = {
+	"off", "absolute", "relative", "hybrid", NULL
+};
 
 /* Status line format. */
 #define OPTIONS_TABLE_STATUS_FORMAT1 \
@@ -1199,6 +1202,32 @@ const struct options_table_entry options_table[] = {
 	  .flags = OPTIONS_TABLE_IS_STYLE,
 	  .separator = ",",
 	  .text = "Style of selection in copy mode."
+	},
+
+	{ .name = "copy-mode-line-numbers",
+	  .type = OPTIONS_TABLE_CHOICE,
+	  .scope = OPTIONS_TABLE_WINDOW,
+	  .choices = options_table_copy_mode_line_numbers_list,
+	  .default_num = 0,
+	  .text = "Line number mode in copy mode."
+	},
+
+	{ .name = "copy-mode-line-number-style",
+	  .type = OPTIONS_TABLE_STRING,
+	  .scope = OPTIONS_TABLE_WINDOW,
+	  .default_str = "fg=colour243",
+	  .flags = OPTIONS_TABLE_IS_STYLE,
+	  .separator = ",",
+	  .text = "Style of line numbers in copy mode."
+	},
+
+	{ .name = "copy-mode-current-line-number-style",
+	  .type = OPTIONS_TABLE_STRING,
+	  .scope = OPTIONS_TABLE_WINDOW,
+	  .default_str = "fg=yellow",
+	  .flags = OPTIONS_TABLE_IS_STYLE,
+	  .separator = ",",
+	  .text = "Style of current line number in copy mode."
 	},
 
 	{ .name = "fill-character",
