5744021ac96b14ec4dc92753973a1c34b8870649
nicm <nicm>
2026-06-07 09:54:25 +0000

    Allow floating panes to be created partially off the window, based on a change from Michael Grant.

--- cmd-split-window.c
+++ cmd-split-window.c
@@ -109,7 +109,7 @@ cmd_split_window_exec(struct cmd *self, struct cmdq_item *item)
 	else
 		lc = layout_get_tiled_cell(item, args, w, wp, flags, &cause);
 	if (cause != NULL) {
-		cmdq_error(item, "%s", cause);
+		cmdq_error(item, "size or position %s", cause);
 		free(cause);
 		return (CMD_RETURN_ERROR);
 	}
