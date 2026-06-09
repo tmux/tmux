4a3f380fd25a34e79b058e97a156162d5ca6a8f9
nicm <nicm>
2026-06-09 12:24:59 +0000

    Add -f for filter with -a to kill-pane, kill-window, kill-session. GitHub issue 4782.

--- cmd-kill-pane.c
+++ cmd-kill-pane.c
@@ -27,13 +27,17 @@
  */
 
 static enum cmd_retval	cmd_kill_pane_exec(struct cmd *, struct cmdq_item *);
+static enum cmd_retval	cmd_kill_pane_all(struct cmdq_item *, const char *);
+static int		cmd_kill_pane_filter(struct cmdq_item *,
+			    struct session *, struct winlink *,
+			    struct window_pane *, const char *);
 
 const struct cmd_entry cmd_kill_pane_entry = {
 	.name = "kill-pane",
 	.alias = "killp",
 
-	.args = { "at:", 0, 0, NULL },
-	.usage = "[-a] " CMD_TARGET_PANE_USAGE,
+	.args = { "af:t:", 0, 0, NULL },
+	.usage = "[-a] [-f filter] " CMD_TARGET_PANE_USAGE,
 
 	.target = { 't', CMD_FIND_PANE, 0 },
 
@@ -46,21 +50,18 @@ cmd_kill_pane_exec(struct cmd *self, struct cmdq_item *item)
 {
 	struct args		*args = cmd_get_args(self);
 	struct cmd_find_state	*target = cmdq_get_target(item);
+	struct session		*s = target->s;
 	struct winlink		*wl = target->wl;
-	struct window_pane	*loopwp, *tmpwp, *wp = target->wp;
+	struct window_pane	*wp = target->wp;
+	const char		*filter = args_get(args, 'f');
 
-	if (args_has(args, 'a')) {
-		server_unzoom_window(wl->window);
-		TAILQ_FOREACH_SAFE(loopwp, &wl->window->panes, entry, tmpwp) {
-			if (loopwp == wp)
-				continue;
-			server_client_remove_pane(loopwp);
-			layout_close_pane(loopwp);
-			window_remove_pane(wl->window, loopwp);
-		}
-		server_redraw_window(wl->window);
-		return (CMD_RETURN_NORMAL);
+	if (filter != NULL && !args_has(args, 'a')) {
+		cmdq_error(item, "-f only valid with -a");
+		return (CMD_RETURN_ERROR);
 	}
+
+	if (args_has(args, 'a'))
+		return (cmd_kill_pane_all(item, filter));
 
 	if (wp == NULL) {
 		cmdq_error(item, "no active pane to kill");
@@ -68,4 +69,49 @@ cmd_kill_pane_exec(struct cmd *self, struct cmdq_item *item)
 	}
 	server_kill_pane(wp);
 	return (CMD_RETURN_NORMAL);
+}
+
+static enum cmd_retval
+cmd_kill_pane_all(struct cmdq_item *item, const char *filter)
+{
+	struct cmd_find_state	*target = cmdq_get_target(item);
+	struct session		*s = target->s;
+	struct winlink		*wl = target->wl;
+	struct window_pane	*wp = target->wp;
+	struct window_pane	*loopwp, *tmpwp;
+
+	server_unzoom_window(wl->window);
+	TAILQ_FOREACH_SAFE(loopwp, &wl->window->panes, entry, tmpwp) {
+		if (loopwp == wp)
+			continue;
+		if (!cmd_kill_pane_filter(item, s, wl, loopwp, filter))
+			continue;
+		server_client_remove_pane(loopwp);
+		layout_close_pane(loopwp);
+		window_remove_pane(wl->window, loopwp);
+	}
+	server_redraw_window(wl->window);
+	return (CMD_RETURN_NORMAL);
+}
+
+static int
+cmd_kill_pane_filter(struct cmdq_item *item, struct session *s,
+    struct winlink *wl, struct window_pane *wp, const char *filter)
+{
+	struct format_tree	*ft;
+	char			*expanded;
+	int			 flag;
+
+	if (filter == NULL)
+		return (1);
+
+	ft = format_create(cmdq_get_client(item), item, FORMAT_NONE, 0);
+	format_defaults(ft, NULL, s, wl, wp);
+
+	expanded = format_expand(ft, filter);
+	flag = format_true(expanded);
+	free(expanded);
+
+	format_free(ft);
+	return (flag);
 }
