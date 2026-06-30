f8674cc993dd09cb355fe920eb95973198a3b254
nicm <nicm>
2026-06-30 10:07:05 +0000

    Add default features for Ghostty, GitHub issue 5305 from Glib Shpychka.

--- tty-keys.c
+++ tty-keys.c
@@ -1663,6 +1663,8 @@ tty_keys_extended_device_attributes(struct tty *tty, const char *buf,
 		tty_default_features(features, "foot", 0);
 	else if (strncmp(tmp, "WezTerm ", 7) == 0)
 		tty_default_features(features, "WezTerm", 0);
+	else if (strncmp(tmp, "ghostty ", 8) == 0)
+		tty_default_features(features, "ghostty", 0);
 	log_debug("%s: received extended DA %.*s", c->name, (int)*size, buf);
 
 	free(c->term_type);
