e69950a9ca773f3183749f3ccd6ee588880040d8
nicm <nicm>
2026-07-01 06:17:58 +0000

    Add overline for ghostty also, GitHub issue 5309.

--- tty-features.c
+++ tty-features.c
@@ -579,6 +579,7 @@ tty_default_features(int *feat, const char *name, u_int version)
 			      "cstyle,"
 			      "extkeys,"
 			      "focus,"
+			      "overline,"
 			      "hyperlinks,"
 			      "osc7,"
 			      "sync,"
