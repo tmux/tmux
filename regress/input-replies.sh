0f77d9b30a63b70072557e4199d662af205e624d
Nicholas Marriott <nicholas.marriott@gmail.com>
2026-07-04 18:52:53 +0100

    Fix lengths in test.

--- regress/input-replies.sh
+++ regress/input-replies.sh
@@ -7,7 +7,7 @@ TERM=screen
 TMUX="$TEST_TMUX -Ltest -f/dev/null"
 
 $TMUX kill-server 2>/dev/null
-sleep 0.1
+sleep 0.5
 
 TMP=$(mktemp)
 EXP=$(mktemp)
@@ -18,7 +18,7 @@ $TMUX new-session -d -x 80 -y 24 -s replies \; \
 $TMUX set-option -s set-clipboard on || exit 1
 $TMUX set-option -s get-clipboard buffer || exit 1
 printf Hello | $TMUX load-buffer -
-sleep 0.3
+sleep 0.5
 
 exit_status=0
 
@@ -53,7 +53,7 @@ query_timeout()
 
 	$TMUX respawn-window -k -t replies:0 \
 	    "stty raw -echo min 0 time 5; printf '$setup'; printf '$seq'; sleep 0.1; dd bs=1 count=128 2>/dev/null | cat -v >$TMP"
-	sleep 0.7
+	sleep 1
 	printf "%s" "$expected" >"$EXP"
 	cmp "$TMP" "$EXP" || fail "$name"
 }
@@ -62,26 +62,26 @@ query "dsr-ok" '^[[0n' '\033[5n' 4 ''
 query "dsr-cursor" '^[[1;1R' '\033[6n' 6 ''
 query "da-primary" '^[[?1;2c' '\033[c' 7 ''
 query "da-secondary" '^[[>84;0;0c' '\033[>c' 10 ''
-query "decrqm-irm-reset" '^[[4;2$y' '\033[4$p' 8 ''
-query "decrqm-irm-set" '^[[4;1$y' '\033[4$p' 8 '\033[4h'
-query "decrqm-cursor-keys-reset" '^[[?1;2$y' '\033[?1$p' 9 ''
-query "decrqm-cursor-keys-set" '^[[?1;1$y' '\033[?1$p' 9 '\033[?1h'
-query "decrqm-columns" '^[[?3;4$y' '\033[?3$p' 9 ''
-query "decrqm-origin-reset" '^[[?6;2$y' '\033[?6$p' 9 ''
-query "decrqm-origin-set" '^[[?6;1$y' '\033[?6$p' 9 '\033[?6h'
-query "decrqm-wrap-set" '^[[?7;1$y' '\033[?7$p' 9 ''
-query "decrqm-wrap-reset" '^[[?7;2$y' '\033[?7$p' 9 '\033[?7l'
-query "decrqm-cursor-visible-set" '^[[?25;1$y' '\033[?25$p' 10 ''
-query "decrqm-cursor-visible-reset" '^[[?25;2$y' '\033[?25$p' 10 '\033[?25l'
-query "decrqm-mouse-standard-set" '^[[?1000;1$y' '\033[?1000$p' 12 '\033[?1000h'
-query "decrqm-mouse-button-set" '^[[?1002;1$y' '\033[?1002$p' 12 '\033[?1002h'
-query "decrqm-mouse-all-set" '^[[?1003;1$y' '\033[?1003$p' 12 '\033[?1003h'
-query "decrqm-focus-set" '^[[?1004;1$y' '\033[?1004$p' 12 '\033[?1004h'
-query "decrqm-mouse-utf8-set" '^[[?1005;1$y' '\033[?1005$p' 12 '\033[?1005h'
-query "decrqm-mouse-sgr-set" '^[[?1006;1$y' '\033[?1006$p' 12 '\033[?1006h'
-query "decrqm-bracket-paste-set" '^[[?2004;1$y' '\033[?2004$p' 12 '\033[?2004h'
-query "decrqm-theme-updates-set" '^[[?2031;1$y' '\033[?2031$p' 12 '\033[?2031h'
-query "decrqss-cursor-style" '^[P1$r q0 q' '\033P$q q\033\\' 10 ''
+query "decrqm-irm-reset" '^[[4;2$y' '\033[4$p' 7 ''
+query "decrqm-irm-set" '^[[4;1$y' '\033[4$p' 7 '\033[4h'
+query "decrqm-cursor-keys-reset" '^[[?1;2$y' '\033[?1$p' 8 ''
+query "decrqm-cursor-keys-set" '^[[?1;1$y' '\033[?1$p' 8 '\033[?1h'
+query "decrqm-columns" '^[[?3;4$y' '\033[?3$p' 8 ''
+query "decrqm-origin-reset" '^[[?6;2$y' '\033[?6$p' 8 ''
+query "decrqm-origin-set" '^[[?6;1$y' '\033[?6$p' 8 '\033[?6h'
+query "decrqm-wrap-set" '^[[?7;1$y' '\033[?7$p' 8 ''
+query "decrqm-wrap-reset" '^[[?7;2$y' '\033[?7$p' 8 '\033[?7l'
+query "decrqm-cursor-visible-set" '^[[?25;1$y' '\033[?25$p' 9 ''
+query "decrqm-cursor-visible-reset" '^[[?25;2$y' '\033[?25$p' 9 '\033[?25l'
+query "decrqm-mouse-standard-set" '^[[?1000;1$y' '\033[?1000$p' 11 '\033[?1000h'
+query "decrqm-mouse-button-set" '^[[?1002;1$y' '\033[?1002$p' 11 '\033[?1002h'
+query "decrqm-mouse-all-set" '^[[?1003;1$y' '\033[?1003$p' 11 '\033[?1003h'
+query "decrqm-focus-set" '^[[?1004;1$y' '\033[?1004$p' 11 '\033[?1004h'
+query "decrqm-mouse-utf8-set" '^[[?1005;1$y' '\033[?1005$p' 11 '\033[?1005h'
+query "decrqm-mouse-sgr-set" '^[[?1006;1$y' '\033[?1006$p' 11 '\033[?1006h'
+query "decrqm-bracket-paste-set" '^[[?2004;1$y' '\033[?2004$p' 11 '\033[?2004h'
+query "decrqm-theme-updates-set" '^[[?2031;1$y' '\033[?2031$p' 11 '\033[?2031h'
+query "decrqss-cursor-style" '^[P1$r q0 q^[\' '\033P$q q\033\\' 12 ''
 
 query_timeout "osc-10-query" '^[]10;rgb:ffff/0000/0000^G' '\033]10;?\007' '\033]10;red\007'
 query_timeout "osc-11-query" '^[]11;rgb:0000/0000/ffff^G' '\033]11;?\007' '\033]11;blue\007'
