5b6498ace44094598a900a7fd3fd9a7f8cb2fa9b
Dane Jensen <dhcjensen@gmail.com>
2026-07-11 23:17:31 -0700

    Updated the layout string format to be more readable and include flags.

--- tmux.h
+++ tmux.h
@@ -3812,7 +3812,7 @@ struct layout_cell *layout_search_by_border(struct layout_cell *, u_int, u_int);
 void		 layout_set_size(struct layout_cell *, u_int, u_int, int, int);
 void		 layout_make_leaf(struct layout_cell *, struct window_pane *);
 void		 layout_make_node(struct layout_cell *, enum layout_type);
-void		 layout_fix_zindexes(struct window *, struct layout_cell *);
+void		 layout_fix_zindexes(struct window *);
 int		 layout_cell_is_tiled(struct layout_cell *);
 int		 layout_add_horizontal_border(struct layout_cell *,
 		     struct layout_cell *, int);
