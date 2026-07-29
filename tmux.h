201db16a0194f417131d7f73383175db9c83df66
David Marte <redesigndavid@gmail.com>
2026-07-29 22:14:24 +0800

    chore: clean up comments

--- tmux.h
+++ tmux.h
@@ -1160,7 +1160,6 @@ enum pane_lines {
 #define PANE_BORDER_ARROWS 2
 #define PANE_BORDER_BOTH 3
 
-/* Pane border type option. */
 #define PANE_BORDER_TYPE_JOINED 0
 #define PANE_BORDER_TYPE_SEPARATE 1
 #define PANE_BORDER_TYPE_SEPARATE_ACTIVE 2
