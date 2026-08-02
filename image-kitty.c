4c04fc805e121f6a18bc70b843b85aebfcbf5dc0
Michael Grant <mgrant@grant.org>
2026-08-02 10:38:26 +0100

    Fix copyrights.

--- image-kitty.c
+++ image-kitty.c
@@ -1,7 +1,7 @@
 /* $OpenBSD$ */
 
 /*
- * Copyright (c) 2026 Nicholas Marriott <nicholas@roaima.co.uk>
+ * Copyright (c) 2026 Michael Grant <mgrant@grant.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
