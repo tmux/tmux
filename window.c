c9f5ecb4b55dcdb80af914eb2320f71f0f1234ec
nicm <nicm@openbsd.org>
2026-08-24 07:14:54 +0000

    Do not leak cached status line, from Jeong, Heon.

--- window.c
+++ window.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: window.c,v 1.371 2026/08/24 07:10:38 nicm Exp $ */
+/* $OpenBSD: window.c,v 1.372 2026/08/24 07:14:54 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1543,6 +1543,7 @@ window_pane_free(struct window_pane *wp)
 	cmd_free_argv(wp->argc, wp->argv);
 	colour_palette_free(&wp->palette);
 	style_ranges_free(&wp->border_status_line.ranges);
+	free(wp->border_status_line.expanded);
 	free(wp);
 }
 
