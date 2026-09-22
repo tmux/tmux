de6912203445f6af963de6fc05c7e5b321fe25b1
nicm <nicm@openbsd.org>
2026-09-22 14:10:26 +0000

    Some terminals have extended DECSCUSR to allow it to reset to the default style and put that as the Se capability in their terminfo entry. Change so that if Ss and Se exist, tmux does not overwrite them with its own. GitHub issue 5476.

--- tmux.h
+++ tmux.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: tmux.h,v 1.1445 2026/09/22 06:46:50 nicm Exp $ */
+/* $OpenBSD: tmux.h,v 1.1447 2026/09/22 14:10:26 nicm Exp $ */
 
 /*
  * Copyright (c) 2007 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -1738,6 +1738,7 @@ struct tty_term {
 #define TERM_VT100LIKE 0x20
 #define TERM_SIXEL 0x40
 #define TERM_INVALIDMS 0x80
+#define TERM_NOREPLACE 0x100
 	int		 flags;
 
 	LIST_ENTRY(tty_term) entry;
@@ -3007,7 +3008,7 @@ void	tty_default_colours(struct grid_cell *, struct window_pane *, u_int *);
 /* tty-term.c */
 extern struct tty_terms tty_terms;
 u_int		 tty_term_ncodes(void);
-void		 tty_term_apply(struct tty_term *, const char *, int);
+void		 tty_term_apply(struct tty_term *, const char *, int, int);
 void		 tty_term_apply_overrides(struct tty_term *);
 struct tty_term *tty_term_create(struct tty *, char *, char **, u_int, char **);
 void		 tty_term_free(struct tty_term *);
--- tty-term.c
+++ tty-term.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: tty-term.c,v 1.109 2026/08/25 08:37:08 nicm Exp $ */
+/* $OpenBSD: tty-term.c,v 1.111 2026/09/22 14:10:26 nicm Exp $ */
 
 /*
  * Copyright (c) 2008 Nicholas Marriott <nicholas.marriott@gmail.com>
@@ -362,7 +362,8 @@ tty_term_override_next(const char *s, size_t *offset)
 }
 
 void
-tty_term_apply(struct tty_term *term, const char *capabilities, int quiet)
+tty_term_apply(struct tty_term *term, const char *capabilities, int quiet,
+    int flags)
 {
 	const struct tty_term_code_entry	*ent;
 	struct tty_code				*code;
@@ -405,6 +406,9 @@ tty_term_apply(struct tty_term *term, const char *capabilities, int quiet)
 			if (strcmp(s, ent->name) != 0)
 				continue;
 			code = &term->codes[i];
+			if ((flags & TERM_NOREPLACE) &&
+			    code->type != TTYCODE_NONE)
+				continue;
 
 			if (remove) {
 				code->type = TTYCODE_NONE;
@@ -457,7 +461,7 @@ tty_term_apply_overrides(struct tty_term *term)
 		offset = 0;
 		first = tty_term_override_next(s, &offset);
 		if (first != NULL && fnmatch(first, term->name, 0) == 0)
-			tty_term_apply(term, s + offset, 0);
+			tty_term_apply(term, s + offset, 0, 0);
 		a = options_array_next(a);
 	}
 
